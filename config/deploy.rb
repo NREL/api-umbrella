f2e99b7cc3fa73ca2c61cd7efe62c5a984936142
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-01-13 03:55:54 +0000

    Add supervisord-based launching and management of auth_proxy. I think this should theoretically be a lot simpler than the previous monit and wrapper scripts.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,38 +3,12 @@ require "capistrano/ext/multistage"
 
 require "capistrano_nrel_ext/recipes/defaults"
 require "capistrano_nrel_ext/recipes/haproxy"
-require "capistrano_nrel_ext/recipes/server_ports"
+require "capistrano_nrel_ext/recipes/supervisor"
 
 # Set the application being deployed.
 set :application, "developer_router"
 
-set(:server_registry) do
-  {
-    # Frontend HAProxy servers.
-    :public_router => [
-      { :host => domain, :port => 80 },
-    ],
-    :api_router => [
-      { :host => "127.0.0.1", :port => 50000 },
-    ],
-
-    # The authentication proxymachine server.
-    :auth_proxy => [
-      { :host => "127.0.0.1", :port => free_server_port },
-    ],
-
-    # The public site.
-    :public_site => [
-      { :host => "127.0.0.1", :port => 50100 },
-    ],
-
-    # API backends.
-    # For local services, start assigning ports in the 50500+ range.
-    :api_sfv => [
-      { :host => "127.0.0.1", :port => 50500 },
-    ],
-    :api_georeserv => [
-      { :host => "rosselli.nrel.gov", :port => 8010 },
-    ],
-  }
-end
+# Bundle gems for the auth_proxy app.
+set :gem_bundler_apps, [
+  "auth_proxy",
+]
