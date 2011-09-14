8f02058e936951cbb5eac5d0bc3aa6727befc1f9
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-09-14 21:26:55 +0000

    Authenticate against CAS on developer servers.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -191,7 +191,17 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :cas, :cas_server => "http://auth.dardev.nrel.gov/cas"
+
+  cas_server = case Rails.env
+  when "development"
+    "https://devdev-new.nrel.gov/auth"
+  when "staging_int"
+    "https://devstage-int.nrel.gov/auth"
+  else
+    "https://developer.nrel.gov/auth"
+  end
+
+  config.omniauth :cas, :cas_server => cas_server, :disable_ssl_verification => true
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
