7b400f98de444fc379de841bee7a3d867856b6db
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-09-21 20:27:05 +0000

    Add production deployment setup.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,21 +1,21 @@
 require "capistrano_nrel_ext/recipes/branches"
 
 # Set the servers for this stage.
-role :app, "devstage-int.nrel.gov"
-role :web, "devstage-int.nrel.gov"
+role :app, "devprod-int.nrel.gov"
+role :web, "devprod-int.nrel.gov"
 
 # Define the primary db server as our app server so database migrations can run
 # from the code checkout there.
-role :db, "devstage-int.nrel.gov", :primary => true
+role :db, "devprod-int.nrel.gov", :primary => true
 
 # On our real database server, don't actually perform a code deployment.
-role :db, "devstage-int-db.nrel.gov", :no_release => true
+role :db, "devprod-int-db.nrel.gov", :no_release => true
 
 # Set the base path for deployment.
-set :deploy_to_base, "/srv/developer/devstage-int"
+set :deploy_to_base, "/srv/developer/devprod-int"
 
 # Set the accessible web domain for this site.
-set :base_domain, "devstage-int.nrel.gov"
+set :base_domain, "devprod-int.nrel.gov"
 
 # Set the Rails environment.
-set :rails_env, "staging_int"
+set :rails_env, "production"
