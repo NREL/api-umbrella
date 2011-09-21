4823655607955c56e99d845e109d2793ba8af4f1
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-09-21 19:58:53 +0000

    Add deployment production.

---
+++ config/deploy/production.rb
@@ -0,0 +1,12 @@
+# Set the servers for this stage.
+role :app, "devprod-int.nrel.gov"
+role :web, "devprod-int.nrel.gov"
+
+# Set the base path for deployment.
+set :deploy_to_base, "/srv/developer/devprod-int"
+
+# Set the accessible web domain for this site.
+set :base_domain, "devprod-int.nrel.gov"
+
+# Set the Rails environment.
+set :rails_env, "production"
