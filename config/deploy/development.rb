136971b1742c6599d901c59cd9f80b53c8f45741
Muerdter, Nick <nick.muerdter@nrel.gov>
2011-10-25 23:55:37 +0000

    Change devdev-new.nrel.gov domain to devdev.nrel.gov. Also introduce subdomains for opsview and the chef web interface.

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,12 +1,12 @@
 # Set the servers for this stage.
-role :app, "devdev-new.nrel.gov"
-role :web, "devdev-new.nrel.gov"
+role :app, "devdev.nrel.gov"
+role :web, "devdev.nrel.gov"
 
 # Set the base path for deployment.
 set :deploy_to_base, "/srv/developer/devdev"
 
 # Set the accessible web domain for this site.
-set :base_domain, "devdev-new.nrel.gov"
+set :base_domain, "devdev.nrel.gov"
 
 # Reduce the number of copies kept since this is the development environment.
 set :keep_releases, 5
