f90056d91d05fea53184ddf0d64304b6763c190d
Muerdter, Nick <nick.muerdter@nrel.gov>
2012-06-26 06:09:33 +0000

    Let's try a branched deployment on our production server for a GSA demo.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,7 @@
+# Force a branched deployment on the production server for the GSA demo.
+ENV["BRANCH"] = "gsa"
+require "capistrano_nrel_ext/recipes/branches"
+
 # Set the servers for this stage.
 role :app, "devprod-int.nrel.gov"
 role :web, "devprod-int.nrel.gov"
