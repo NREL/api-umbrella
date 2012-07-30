a496a64a9dc5898922f3e3a49395402c618bf74a
Muerdter, Nick <nick.muerdter@nrel.gov>
2012-07-30 20:43:04 +0000

    Add basic methods for validating API keys via a service.

--- app/controllers/api/api_users_controller.rb
+++ app/controllers/api/api_users_controller.rb
@@ -1,4 +1,27 @@
 class Api::ApiUsersController < ApplicationController
+  def validate
+    @user = ApiUser.where(:api_key => params[:id]).first
+
+    @response = { :status => "invalid" }
+    if @user
+      @response[:status] = "valid"
+    end
+
+    respond_to do |format|
+      format.json { render :json => @response }
+      format.xml { render :xml => @response }
+    end
+  end
+
+  def show
+    @user = ApiUser.where(:api_key => params[:id]).first
+
+    respond_to do |format|
+      format.json { render :json => @user }
+      format.xml { render :xml => @user }
+    end
+  end
+
   def create
     @user = ApiUser.find_or_initialize_by(:email => params[:email], :website => params[:website])
     @user.no_domain_signup = true
