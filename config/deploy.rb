30ba32138f5cabd8a376767a58199c5842c2d8b5
Nick Muerdter <nick.muerdter@nrel.gov>
2012-08-22 19:41:15 -0600

    Let's try deploying from git.

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,19 @@ require "capistrano_nrel_ext/recipes/redhat"
 # Set the application being deployed.
 set :application, "developer"
 
+set :scm, "git"
+set(:repository) { "git@github.com:NREL/#{application}.git" }
+set(:branch) { branch_name || "HEAD" }
+set :deploy_via, :copy
+set :copy_cache, true
+set :copy_exclude, ".git/*"
+
+if(File.exists?("/usr/bin/gnutar"))
+  set :copy_local_tar, "gnutar"
+end
+
+ssh_options[:forward_agent] = true
+
 # Define the rails-based applications.
 set :rails_app_paths,  {
   "." => "/",
