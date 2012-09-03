760b6b65d411001f74cb7a5da8f7816a8b243ff5
Nick Muerdter <nick.muerdter@nrel.gov>
2012-09-03 08:33:11 -0600

    Namespace the models.

--- app/models/api_request_log.rb
+++ app/models/api_request_log.rb
@@ -1,11 +1,15 @@
-class ApiRequestLog
-  include Mongoid::Document
+require "mongoid"
 
-  field :api_key
-  field :path
-  field :ip_address
-  field :requested_at, :type => Time
-  field :response_status, :type => Integer
-  field :response_error
-  field :env
+module ApiUmbrella
+  class ApiRequestLog
+    include Mongoid::Document
+
+    field :api_key
+    field :path
+    field :ip_address
+    field :requested_at, :type => Time
+    field :response_status, :type => Integer
+    field :response_error
+    field :env
+  end
 end
