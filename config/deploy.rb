749b1e14f238a188c3c0d1690783b405c71b020d
Nick Muerdter <nick.muerdter@nrel.gov>
2012-09-03 20:55:45 -0600

    Update config files to reflect new Gatekeeper usage.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,8 +9,3 @@ require "capistrano_nrel_ext/recipes/supervisor"
 
 # Set the application being deployed.
 set :application, "developer_router"
-
-# Bundle gems for the auth_proxy app.
-set :gem_bundler_apps, [
-  "auth_proxy",
-]
