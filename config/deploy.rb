3eef710f14fe7641f071a7688d651cb3accb867c
Nick Muerdter <nick.muerdter@nrel.gov>
2012-09-08 10:39:41 -0600

    Try to get deployments working again against the new projects and Vagrant.

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,4 +8,10 @@ require "capistrano_nrel_ext/recipes/nginx"
 require "capistrano_nrel_ext/recipes/supervisor"
 
 # Set the application being deployed.
-set :application, "developer_router"
+set :application, "api-umbrella-router"
+
+set :scm, "git"
+set :branch, "master"
+set(:repository) { "http://github.com/NREL/#{application}.git" }
+
+ssh_options[:forward_agent] = true
---
+++ config/deploy/vagrant.rb
@@ -0,0 +1,27 @@
+# Set the servers for this stage.
+role :app, "api.vagrant"
+role :web, "api.vagrant"
+
+# Set the base path for deployment.
+set :deploy_to_base, "/srv/sites"
+
+set(:release_name) { application }
+set(:releases_path) { File.join("/vagrant/workspace") }
+set(:releases) { [application] }
+
+# Set the accessible web domain for this site.
+set :base_domain, "api.vagrant"
+
+# Only maintain a single release and checkout for our local development. This
+# means there's no ability to rollback releases.
+set :deploy_via, :cached_checkout
+
+# Set the Rails environment.
+set :rails_env, "development"
+
+# Set gem bundler options for the development environment.
+set :bundle_without, [:test]
+set :bundle_flags, "--quiet"
+
+set :group_writable, false
+set :disable_internal_symlinks, true
