8780ce693c6ecc263e32150333d743d77f14afde
Nick Muerdter <nick.muerdter@nrel.gov>
2012-12-28 17:01:22 -0700

    Bump Ruby to 1.9.3-p352 and Bundler to 1.2.3.

--- chef/roles/ruby.rb
+++ chef/roles/ruby.rb
@@ -12,20 +12,20 @@ default_attributes({
   :rbenv => {
     # Don't use the git:// protocol behind our firewall.
     :git_repository => "http://github.com/sstephenson/rbenv.git",
-    :git_revision => "6778c8e905d774d4dc70724c455e6fcff4c1d3e1",
-    :install_global_version => "1.9.3-p194",
+    :git_revision => "c3fe192243bff9a00866d81af38d9012bfba419a",
+    :install_global_version => "1.9.3-p362",
   },
   :ruby_build => {
     # Don't use the git:// protocol behind our firewall.
     :git_repository => "http://github.com/sstephenson/ruby-build.git",
-    :version => "20120815",
-    :git_revision => "v20120815",
+    :version => "20121227",
+    :git_revision => "v20121227",
   },
   :rubygems => {
     :version => "1.8.24",
     :default_options => "--no-ri --no-rdoc",
   },
   :bundler => {
-    :version => "1.1.5",
+    :version => "1.2.3",
   },
 })
