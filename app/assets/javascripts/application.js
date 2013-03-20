4f1396c73ac536ed6a6e9104b9edb7a082bc8937
Nick Muerdter <nick.muerdter@nrel.gov>
2013-03-20 14:16:49 -0600

    Fix client_side_validations for IE8.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,7 @@
+// indexOf polyfill needed for client_side_validation compatibility with IE8:
+// https://github.com/bcardarella/client_side_validations/issues/499#issuecomment-14498451
+//= require polyfills/index_of
+
 //= require jquery_ujs
 //= require rails.validations
 //= require rails.validations.simple_form
