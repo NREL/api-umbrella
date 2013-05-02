ff151d7ee95cc28228a19287ed57cb72e9bf7299
Nick Muerdter <nick.muerdter@nrel.gov>
2013-05-01 23:26:58 -0600

    Some demo deployment setup.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,21 @@
 # Set the servers for this stage.
-role :app, "devprod-int.nrel.gov"
-role :web, "devprod-int.nrel.gov"
+role :app, "api-web1.anydns.com"
+role :web, "api-web1.anydns.com"
 
 # Set the base path for deployment.
-set :deploy_to_base, "/srv/data/devprod-int"
+set :deploy_to_base, "/srv"
 
 # Set the accessible web domain for this site.
-set :base_domain, "devprod-int.nrel.gov"
+set :base_domain, "api-web1.anydns.com"
+
+# Production-ready deployments should exclude git data.
+set :copy_exclude, [".git"]
 
 # Set the Rails environment.
 set :rails_env, "production"
+
+set :branch, "node"
+set :user, "root"
+ssh_options[:keys] = ["/vagrant/workspace/aws_nmuerdter.pem"]
+
+set :ssl_cert_pem, "/etc/ssl/certs/vagrant.pem"
