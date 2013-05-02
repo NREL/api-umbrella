2f3ea5fd3d7d2bb546042da3339ea5985408a358
Nick Muerdter <nick.muerdter@nrel.gov>
2013-05-01 23:34:46 -0600

    Deploy to both web servers.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,12 @@
 # Set the servers for this stage.
-role :app, "api-web1.anydns.com"
-role :web, "api-web1.anydns.com"
+role :app, "api-web1.anydns.com", "api-web2.anydns.com"
+role :web, "api-web1.anydns.com", "api-web2.anydns.com"
 
 # Set the base path for deployment.
 set :deploy_to_base, "/srv"
 
 # Set the accessible web domain for this site.
-set :base_domain, "api-web1.anydns.com"
+set :base_domain, "api.anydns.com"
 
 # Production-ready deployments should exclude git data.
 set :copy_exclude, [".git"]
