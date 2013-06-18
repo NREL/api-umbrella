442dbc69d8c06a1e2e87e593f508753847abd45e
Nick Muerdter <nick.muerdter@nrel.gov>
2013-06-18 09:15:08 -0600

    Bump nodejs to 0.10.11.

--- chef/roles/nodejs.rb
+++ chef/roles/nodejs.rb
@@ -10,8 +10,8 @@ default_attributes({
   :nodejs => {
     :install_method => "binary",
     :dir => "/opt/nodejs",
-    :version => "0.10.10",
-    :checksum_linux_x64 => "ab42335b0e6e45bac62823d995d8062e9ba0344bc416c76a263a5e45773b2e7d",
+    :version => "0.10.11",
+    :checksum_linux_x64 => "0fa2be9b44d6acd4bd43908bade00053de35e6e27f72a2dc41d072c86263b52a",
   },
 })
 
