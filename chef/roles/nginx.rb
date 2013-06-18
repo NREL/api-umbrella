23f68b03025422961eee89be15a6ec7fc7687cf9
Nick Muerdter <nick.muerdter@nrel.gov>
2013-06-18 09:14:37 -0600

    Bump nginx to 1.2.9.

--- chef/roles/nginx.rb
+++ chef/roles/nginx.rb
@@ -9,11 +9,12 @@ default_attributes({
   :nginx => {
     :install_method => "source",
 
-    :version => "1.2.8",
+    :version => "1.2.9",
 
     :user => "www-data-local",
 
     :source => {
+      :checksum => "71486674b757f6aa93a4f7ec6e68ef82557d4944deeb08f3ad3de00079d22b1c",
       :modules => [
         "http_realip_module",
         "http_stub_status_module",
