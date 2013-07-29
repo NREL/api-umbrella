118c3a1467110a4997ac7ab706098c5870786f8e
Nick Muerdter <nick.muerdter@nrel.gov>
2013-07-29 12:51:52 -0600

    Switch to using nginx instead of haproxy as the main router.
    This is due to HAProxy's current lack of server-side keep alive support. It's
    coming in HAProxy 1.5, but the feature has yet to land, so at least for now
    we're switching to nginx. HAProxy may have more robust load balancing and
    proxying capabilities, but the server-side keep-alive feature actually improves
    the performance quite a bit for our main use-case of remote backends. While it
    doesn't make much of a difference if all your servers are local, since we're
    routing to servers all over the place, being able to hold open a pool of
    cached, persistent connections for a short time speeds things up a fair amount
    (I've experienced improvements of 10-100ms per request).
    
    We could revisit once this functionality lands in HAProxy 1.5, since HAProxy
    still has a lot of powerful features that might be nice to take advantage of.
    But nginx's config syntax makes some things a lot easier, so that's also a nice
    bonus of the switch to take into account.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ require "capistrano/ext/multistage"
 require "capistrano_nrel_ext/recipes/defaults"
 require "capistrano_nrel_ext/recipes/gem_bundler"
 require "capistrano_nrel_ext/recipes/npm"
-require "capistrano_nrel_ext/recipes/haproxy"
+require "capistrano_nrel_ext/recipes/nginx"
 require "capistrano_nrel_ext/recipes/supervisor"
 
 # Set the application being deployed.
