671eaaa115fd51e18596d5b74bb478be2967bc10
Nick Muerdter <nick.muerdter@nrel.gov>
2013-09-08 12:03:21 -0600

    Setup headers model.

--- app/models/api/header.rb
+++ app/models/api/header.rb
@@ -1,3 +1,10 @@
 class Api::Header
   include Mongoid::Document
+
+  # Fields
+  field :key, :type => String
+  field :value, :type => String
+
+  # Mass assignment security
+  attr_accessible :_id, :key, :value
 end
