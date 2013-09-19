aa7e7ecfe00edcb06ac54d13f4203a10142a552d
Nick Muerdter <nick.muerdter@nrel.gov>
2013-09-19 15:52:02 -0600

    Reduce mongodb resource usage on arbiter.

--- chef/roles/mongodb_arbiter.rb
+++ chef/roles/mongodb_arbiter.rb
@@ -8,4 +8,9 @@ run_list([
 ])
 
 default_attributes({
+  :mongodb => {
+    # Don't enable journaling on arbiters, since it's unneeded and leads to the
+    # consumption of 3GB of disk: https://jira.mongodb.org/browse/SERVER-3831
+    :nojournal => true,
+  },
 })
