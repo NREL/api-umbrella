28e0d8e57db5f129f765ca5e0df179f5b3935914
Nick Muerdter <nick.muerdter@nrel.gov>
2013-09-19 20:03:35 -0600

    Add a flash confirmation to the publish process.

--- app/controllers/admin/config_controller.rb
+++ app/controllers/admin/config_controller.rb
@@ -4,6 +4,8 @@ class Admin::ConfigController < Admin::BaseController
 
   def create
     ConfigVersion.publish!
+
+    flash[:success] = "Successfully published configuration... Changes should be live in a few seconds..."
     redirect_to(admin_config_publish_path)
   end
 end
