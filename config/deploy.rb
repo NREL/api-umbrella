1385197cde491310a4b1a6ccf96c4f408d1bd9a6
Nick Muerdter <nick.muerdter@nrel.gov>
2013-09-19 22:12:48 -0600

    Try to fix some issues around the generated runtime.yml between deploys.

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,11 +25,16 @@ set :shared_children_files, %w(config/gatekeeper/runtime.yml config/gatekeeper/r
 set :writable_children_dirs, %w(config/gatekeeper)
 
 after "deploy:setup", "deploy:app:setup_dirs"
+after "deploy:finalize_permissions", "deploy:app:finalize_permissions"
 
 namespace :deploy do
   namespace :app do
     task :setup_dirs, :except => { :no_release => true } do
       run "#{try_sudo} mkdir -p #{File.join(shared_path, "log/gatekeeper")}"
+    end
+
+    task :finalize_permissions, :except => { :no_release => true } do
+      run "setfacl -R -m 'u:api-umbrella-gatekeeper:rwx' #{File.join(shared_path, "config/gatekeeper")}"
     end
   end
 end
