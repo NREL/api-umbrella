7316cb5eec0202b257b22a5d1e0c0de77f2e571c
Nick Muerdter <nick.muerdter@nrel.gov>
2013-11-06 21:10:52 -0700

    Switch back to our redis cookbook with fixes for installing 2.6.16.
    The redisio cookbook requires more changes to existing installations
    than I want to deal with right now, so revert back to our simple
    cookbook so upgrades of existing servers are easier.

--- chef/roles/redis_server.rb
+++ chef/roles/redis_server.rb
@@ -2,15 +2,11 @@ name "redis_server"
 description "A minimal role for all redis servers."
 
 run_list([
-  "recipe[redisio::install]",
-  "recipe[redisio::enable]",
+  "recipe[redis]",
 ])
 
 default_attributes({
-  :redisio => {
-    :mirror => "http://download.redis.io/releases",
+  :redis => {
     :version => "2.6.16",
-    :install_dir => "/opt/redis",
-    :safe_install => false,
   },
 })
