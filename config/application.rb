f1f7e7469ad49c317e052d3e61fffd82e532f8d9
Nick Muerdter <nick.muerdter@nrel.gov>
2013-11-25 08:17:47 -0700

    Move models to root directory.
    The shared directory was a vestige of the old proxymachine based setup
    that's no longer in affect.

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Developer
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/app/models/shared)
+    # config.autoload_paths += %W(#{config.root}/app/models/shared)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
