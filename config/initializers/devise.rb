1ba24e04b5fa1ff61382abeb745d31115543c101
Nick Muerdter <nick.muerdter@nrel.gov>
2013-11-26 13:56:02 -0700

    Better setup for using different secret keys on real servers.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,13 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  config.secret_key = 'fbafe6a3663ac3f7b43000ff02c4cf0f64019007113ffa88606e1a41c2ff458b322384d79c362bf116eb777d2db3fab8848fd9020f9e8bdb211677ed91f14b27'
+  development_secret_key = "fbafe6a3663ac3f7b43000ff02c4cf0f64019007113ffa88606e1a41c2ff458b322384d79c362bf116eb777d2db3fab8848fd9020f9e8bdb211677ed91f14b27"
+  config.secret_key = ENV["DEVISE_SECRET_KEY"] || development_secret_key
+  if(!%w(development test).include?(Rails.env))
+    if(config.secret_key == development_secret_key)
+      raise "An insecure secret token is being used. Please set the DEVISE_SECRET_KEY environment variable with your own private key. Run 'rake secret_keys:generate' for more details."
+    end
+  end
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Developer::Application.config.secret_token = '27f8bb481eebc6a51a88b10582798a5a8ce187ebd4a1e6882493ee3590fe53e01367133d8e2f8806dc8cee6436180460c54862cf3bc72f43eca515c885d29777'
+development_secret_token = "27f8bb481eebc6a51a88b10582798a5a8ce187ebd4a1e6882493ee3590fe53e01367133d8e2f8806dc8cee6436180460c54862cf3bc72f43eca515c885d29777"
+Developer::Application.config.secret_token = ENV["RAILS_SECRET_TOKEN"] || development_secret_token
+if(!%w(development test).include?(Rails.env))
+  if(Developer::Application.config.secret_token == development_secret_token)
+    raise "An insecure secret token is being used. Please set the RAILS_SECRET_TOKEN environment variable with your own private key. Run 'rake secret_keys:generate' for more details."
+  end
+end
