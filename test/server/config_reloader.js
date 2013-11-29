d4cc44aafe5d74da67939c4a96a05846a9c44e9b
Nick Muerdter <nick.muerdter@nrel.gov>
2013-11-29 11:58:13 -0700

    Improve DNS resolver. Use system DNS servers and persist the results.
    We were already caching the results in memory, but persisting the results in
    redis helps cut down on the need for nginx to reload when the gatekeeper gets
    restarted.

--- test/server/config_reloader.js
+++ test/server/config_reloader.js
@@ -6,6 +6,14 @@ var ipaddr = require('ipaddr.js');
 
 describe('config reloader', function() {
   describe('dns resolver', function() {
+    before(function(done) {
+      redisClient.set('router_active_ip:once-valid.blah', '1.2.3.4', function() {
+        redisClient.set('router_active_ip:google.com', '5.6.7.8', function() {
+          done();
+        });
+      });
+    });
+
     shared.runConfigReloader({
       apis: [
         {
@@ -26,15 +34,42 @@ describe('config reloader', function() {
             },
           ],
         },
+        {
+          _id: 'invalid-api',
+          servers: [
+            {
+              host: 'foo.blah',
+              port: 80,
+            },
+          ],
+        },
+        {
+          _id: 'once-valid-api',
+          servers: [
+            {
+              host: 'once-valid.blah',
+              port: 80,
+            },
+          ],
+        },
+        {
+          _id: 'cached-api',
+          servers: [
+            {
+              host: 'google.com',
+              port: 80,
+            },
+          ],
+        },
       ],
     });
 
     it('resolves server host names', function() {
       var block = this.nginxConfigContents.match(/upstream api_umbrella_example-api_backend {[^}]*}/)[0];
-      var upstreamIp = block.match(/server (.+):80;/)[1];
+      var ip = block.match(/server (.+):80;/)[1];
 
-      upstreamIp.should.not.eql('0.0.0.0');
-      ipaddr.isValid(upstreamIp).should.eql(true);
+      ip.should.not.eql('0.0.0.0');
+      ipaddr.isValid(ip).should.eql(true);
     });
 
     it('leaves ip addresses alone', function() {
@@ -42,6 +77,37 @@ describe('config reloader', function() {
       var upstreamIp = block.match(/server (.+):80;/)[1];
 
       upstreamIp.should.eql('8.8.8.8');
+    });
+
+    it('resolves unknown hosts to 0.0.0.0', function() {
+      var block = this.nginxConfigContents.match(/upstream api_umbrella_invalid-api_backend {[^}]*}/)[0];
+      var upstreamIp = block.match(/server (.+):80;/)[1];
+
+      upstreamIp.should.eql('0.0.0.0');
+    });
+
+    it('caches resolved addresses in redis', function(done) {
+      redisClient.get('router_active_ip:example.com', function(error, ip) {
+        ip.should.not.eql('0.0.0.0');
+        ipaddr.isValid(ip).should.eql(true);
+        done();
+      });
+    });
+
+    it('maintains previously cached addresses even if no longer valid', function() {
+      var block = this.nginxConfigContents.match(/upstream api_umbrella_once-valid-api_backend {[^}]*}/)[0];
+      var upstreamIp = block.match(/server (.+):80;/)[1];
+
+      upstreamIp.should.eql('1.2.3.4');
+    });
+
+    it('replaces the cached values with new values when the domain can be resolved', function() {
+      var block = this.nginxConfigContents.match(/upstream api_umbrella_cached-api_backend {[^}]*}/)[0];
+      var ip = block.match(/server (.+):80;/)[1];
+
+      ip.should.not.eql('5.6.7.8');
+      ip.should.not.eql('0.0.0.0');
+      ipaddr.isValid(ip).should.eql(true);
     });
   });
 });
