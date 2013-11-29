90c4161c0d89853f14119a599f81e8ace750d523
Nick Muerdter <nick.muerdter@nrel.gov>
2013-11-28 19:09:34 -0700

    Add validation to ensure API hostnames are valid.

--- app/models/api/server.rb
+++ app/models/api/server.rb
@@ -1,3 +1,5 @@
+require "resolv"
+
 class Api::Server
   include Mongoid::Document
 
@@ -10,9 +12,24 @@ class Api::Server
   embedded_in :api
 
   # Validations
+  validates :host,
+    :presence => true
   validates :port,
     :inclusion => { :in => 0..65_535 }
+  validate :validate_host_resolves
 
   # Mass assignment security
   attr_accessible :host, :port
+
+  private
+
+  def validate_host_resolves
+    if(self.host.present?)
+      begin
+        Resolv.getaddress(self.host)
+      rescue => error
+        self.errors.add(:host, "Could not resolve host: #{error.message}")
+      end
+    end
+  end
 end
