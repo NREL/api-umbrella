fdeb1466edd80ca2fb4734b99e8923ad994c83a8
Nick Muerdter <nick.muerdter@nrel.gov>
2013-11-29 21:10:54 -0700

    Loosen DNS resolver so it caches and tries to reuse recent IPs.
    As long as an IP has been seen in the past 4 hours, it will continue to
    use it. This slightly breaks DNS TTL, but this was needed to accommodate
    domains that have very short TTLs and cycle through a variety of IPs.
    That was causing new IPs and nginx to be reloaded too frequently. 4
    hours may be overkill, though, so this could be revisited if that turns
    out to be too long.

--- test/server/config_reloader.js
+++ test/server/config_reloader.js
@@ -2,16 +2,33 @@
 
 require('../test_helper');
 
-var ipaddr = require('ipaddr.js');
+var async = require('async'),
+    DnsResolver = require('../../lib/config_reloader/dns_resolver').DnsResolver,
+    ipaddr = require('ipaddr.js'),
+    moment = require('moment');
 
 describe('config reloader', function() {
   describe('dns resolver', function() {
     before(function(done) {
-      redisClient.set('router_active_ip:once-valid.blah', '1.2.3.4', function() {
-        redisClient.set('router_active_ip:google.com', '5.6.7.8', function() {
-          done();
-        });
-      });
+      async.parallel([
+        function(callback) {
+          redisClient.set('router_active_ip:once-valid.blah', '1.2.3.4', callback);
+        },
+        function(callback) {
+          redisClient.set('router_active_ip:google.com', '10.0.0.1', callback);
+        },
+        function(callback) {
+          var key = 'router_seen_ips:google.com:' + moment().subtract(3, 'hours').format(DnsResolver.RECENT_IP_BUCKET_TIME_FORMAT);
+          redisClient.sadd(key, ['10.0.0.1'], callback);
+        },
+        function(callback) {
+          redisClient.set('router_active_ip:yahoo.com', '10.0.0.1', callback);
+        },
+        function(callback) {
+          var key = 'router_seen_ips:yahoo.com:' + moment().subtract(4, 'hours').format(DnsResolver.RECENT_IP_BUCKET_TIME_FORMAT);
+          redisClient.sadd(key, ['10.0.0.1'], callback);
+        },
+      ], done);
     });
 
     shared.runConfigReloader({
@@ -70,6 +87,24 @@ describe('config reloader', function() {
             },
           ],
         },
+        {
+          _id: 'use-recent-api',
+          servers: [
+            {
+              host: 'google.com',
+              port: 80,
+            },
+          ],
+        },
+        {
+          _id: 'recent-expired-api',
+          servers: [
+            {
+              host: 'yahoo.com',
+              port: 80,
+            },
+          ],
+        },
       ],
     });
 
@@ -102,7 +137,7 @@ describe('config reloader', function() {
       upstreamIp.should.eql('127.0.0.1');
     });
 
-    it('caches resolved addresses in redis', function(done) {
+    it('caches the active address in redis', function(done) {
       redisClient.get('router_active_ip:example.com', function(error, ip) {
         ip.should.not.eql('255.255.255.255');
         ipaddr.isValid(ip).should.eql(true);
@@ -110,10 +145,26 @@ describe('config reloader', function() {
       });
     });
 
-    it('does not cache unresolved addresses in redis', function(done) {
+    it('maintains a list of recently seen vaid ips in redis', function(done) {
+      var key = 'router_seen_ips:example.com:' + moment().format(DnsResolver.RECENT_IP_BUCKET_TIME_FORMAT);
+      redisClient.smembers(key, function(error, ips) {
+        ips.length.should.be.greaterThan(0);
+        ipaddr.isValid(ips[0]).should.eql(true);
+        done();
+      });
+    });
+
+    it('caches the active address in redis if unresolved', function(done) {
       redisClient.get('router_active_ip:foo.blah', function(error, ip) {
-        console.info(arguments);
-        should.not.exist(ip);
+        ip.should.eql('255.255.255.255');
+        done();
+      });
+    });
+
+    it('does not cache unresolved addresses in the list of recently seen valid ips', function(done) {
+      var key = 'router_seen_ips:foo.blah:' + moment().format(DnsResolver.RECENT_IP_BUCKET_TIME_FORMAT);
+      redisClient.smembers(key, function(error, ips) {
+        ips.length.should.eql(0);
         done();
       });
     });
@@ -125,11 +176,18 @@ describe('config reloader', function() {
       upstreamIp.should.eql('1.2.3.4');
     });
 
-    it('replaces the cached values with new values when the domain can be resolved', function() {
-      var block = this.nginxConfigContents.match(/upstream api_umbrella_cached-api_backend {[^}]*}/)[0];
+    it('keeps using the same address if it has been seen in the past 4 wall hours', function() {
+      var block = this.nginxConfigContents.match(/upstream api_umbrella_use-recent-api_backend {[^}]*}/)[0];
       var ip = block.match(/server (.+):80;/)[1];
 
-      ip.should.not.eql('5.6.7.8');
+      ip.should.eql('10.0.0.1');
+    });
+
+    it('stops using recently seen address more than 4 wall hours old', function() {
+      var block = this.nginxConfigContents.match(/upstream api_umbrella_recent-expired-api_backend {[^}]*}/)[0];
+      var ip = block.match(/server (.+):80;/)[1];
+
+      ip.should.not.eql('10.0.0.2');
       ip.should.not.eql('255.255.255.255');
       ipaddr.isValid(ip).should.eql(true);
     });
