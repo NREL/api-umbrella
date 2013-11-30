c220af2130bff5cba413697eba66c3d5028b1452
Nick Muerdter <nick.muerdter@nrel.gov>
2013-11-29 22:21:36 -0700

    Spin up a separate redis server for running tests.
    Previously, it was using the main redis database that's also used in
    development, so things were getting overwritten between development and
    test.

--- test/support/database_setup.js
+++ test/support/database_setup.js
@@ -2,8 +2,13 @@
 
 require('../test_helper');
 
-mongoose.connect('mongodb://127.0.0.1:27017/api_umbrella_test');
-var redis = require('redis');
+var async = require('async'),
+    config = require('../../lib/config'),
+    redis = require('redis'),
+    net = require('net'),
+    spawn = require('child_process').spawn;
+
+mongoose.connect(config.get('mongodb'));
 
 // Drop the mongodb database.
 before(function(done) {
@@ -16,9 +21,39 @@ before(function(done) {
   });
 });
 
+// Spin up a new redis-server for running the test suite. Multiple database on
+// the main instance would be an option, but that seems like it may become
+// deprecated. This also ensures our tests don't accidentally step on a
+// person's local usage of any random database on the main redis instance.
+var redisServer;
+before(function(done) {
+  // Spin up the redis-server process.
+  redisServer = spawn('redis-server', ['--port', config.get('redis.port')]);
+
+  // Ensure that the process is killed when the tests end.
+  process.on('exit', function () {
+    redisServer.kill('SIGKILL');
+  });
+
+  // Wait until we're able to establish a connection before moving on.
+  var connected = false;
+  async.until(function() {
+    return connected;
+  }, function(callback) {
+    net.connect({
+      port: config.get('redis.port'),
+    }).on('connect', function() {
+      connected = true;
+      callback();
+    }).on('error', function() {
+      setTimeout(callback, 20);
+    });
+  }, done);
+});
+
 // Wipe the redis data.
 before(function(done) {
-  global.redisClient = redis.createClient();
+  global.redisClient = redis.createClient(config.get('redis'));
   global.redisClient.flushdb(function() {
     done();
   });
@@ -29,11 +64,15 @@ after(function(done) {
   mongoose.connection.close(function() {
     done();
   });
-
 });
 
 after(function(done) {
   global.redisClient.quit(function() {
-    done();
+    redisServer.on('exit', function() {
+      done();
+    });
+
+    // Attempt to gracefully shutdown.
+    redisServer.kill('SIGTERM');
   });
 });
