d1ed345877145db222a5349e1a8f829e71dfa97b
Nick Muerdter <nick.muerdter@nrel.gov>
2014-02-10 10:20:23 -0700

    Use relative paths to hopefully work on github pages sub-paths.

--- config.rb
+++ config.rb
@@ -63,7 +63,8 @@ configure :build do
   activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
+  set :relative_links, true
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
