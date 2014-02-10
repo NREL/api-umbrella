694ccd33bde6cf4f849e455618ed0e58712fced4
Nick Muerdter <nick.muerdter@nrel.gov>
2014-02-10 10:44:31 -0700

    Temporarily disable cache busting to workaround github issues.
    The cache busting seems to interfere with the popup's @2x retina
    detection.

--- config.rb
+++ config.rb
@@ -60,7 +60,7 @@ configure :build do
   activate :minify_javascript
 
   # Enable cache buster
-  activate :asset_hash
+  # activate :asset_hash
 
   # Use relative URLs
   activate :relative_assets
