284b633b8c20f24ecb9216927cce152b438ab83b
Nick Muerdter <nick.muerdter@nrel.gov>
2014-07-12 16:26:41 -0600

    Enable minutely interval button. Check range boundaries.
    We now check to ensure the user hasn't attempted to request too long of
    an interval for hourly or minutely data (since that can cause the
    graphing to lock up).

--- app/assets/javascripts/admin/routes/stats/logs_route.js
+++ app/assets/javascripts/admin/routes/stats/logs_route.js
@@ -7,7 +7,11 @@ Admin.StatsLogsRoute = Admin.StatsBaseRoute.extend({
 
   model: function(params) {
     this._super(params);
-    return Admin.StatsLogs.find(this.get('query.params'));
+    if(this.validateOptions()) {
+      return Admin.StatsLogs.find(this.get('query.params'));
+    } else {
+      return {};
+    }
   },
 
   queryChange: function() {
@@ -19,6 +23,36 @@ Admin.StatsLogsRoute = Admin.StatsBaseRoute.extend({
       }
     }
   }.observes('query.params.search', 'query.params.interval', 'query.params.start', 'query.params.end'),
+
+  validateOptions: function() {
+    var valid = true;
+
+    var interval = this.get('query.params.interval');
+    var start = moment(this.get('query.params.start'));
+    var end = moment(this.get('query.params.end'));
+
+    var range = end.unix() - start.unix();
+    switch(interval) {
+      case 'minute':
+        // 2 days maximum range
+        if(range > 2 * 24 * 60 * 60) {
+          valid = false;
+          bootbox.alert('Your date range is too large for viewing minutely data. Adjust your viewing interval or choose a date range to no more than 2 days.')
+        }
+
+        break;
+      case 'hour':
+        // 31 day maximum range
+        if(range > 31 * 24 * 60 * 60) {
+          valid = false;
+          bootbox.alert('Your date range is too large for viewing hourly data. Adjust your viewing interval or choose a date range to no more than 31 days.')
+        }
+
+        break;
+    }
+
+    return valid;
+  },
 });
 
 Admin.StatsLogsDefaultRoute = Admin.StatsLogsRoute.extend({
