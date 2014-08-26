92bf6542b786e4038f63569dec5e451292f69ac0
Nick Muerdter <nick.muerdter@nrel.gov>
2014-08-26 15:04:37 -0400

    Fix logging test failures due to redis-convoy config errors in test env.

--- test/support/env.js
+++ test/support/env.js
@@ -4,3 +4,8 @@ var path = require('path');
 
 process.env.NODE_ENV = 'test';
 process.env.NODE_LOG_DIR = path.resolve(__dirname, '../log');
+
+// redis-convoy uses the config module and requires a bit more setup when in
+// the test environment (the defaults don't get loaded if NODE_ENV=test.
+process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '../config');
+process.env.NODE_RUNTIME_CONFIG_DIR = process.env.NODE_CONFIG_DIR;
