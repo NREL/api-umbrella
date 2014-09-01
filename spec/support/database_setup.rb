61188f5b0bb819c4283a57e2767dff56a38925a0
Nick Muerdter <nick.muerdter@nrel.gov>
2014-09-01 13:40:00 -0600

    Try to sort out database seeding on travis.

--- spec/support/database_setup.rb
+++ spec/support/database_setup.rb
@@ -2,5 +2,6 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner[:mongoid].strategy = :truncation
     DatabaseCleaner.clean
+    Rails.application.load_seed
   end
 end
--- spec/support/db_seed.rb
+++
@@ -1,5 +0,0 @@
-RSpec.configure do |config|
-  config.before(:suite) do
-    Rails.application.load_seed
-  end
-end
