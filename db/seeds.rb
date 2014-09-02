b25fd136b2b7ae3373e0cd0c0e22d14b5ebe67c0
Nick Muerdter <nick.muerdter@nrel.gov>
2014-09-01 23:01:47 -0600

    Remove defunct API backend from seed file.
    This backend is now handled internally inside the router's file
    configuration.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,3 @@
-api = Api.find_or_initialize_by(:name => 'API Umbrella - Default')
-api.assign_attributes({
-  :frontend_host => ConfigSettings.default_host,
-  :backend_host => ConfigSettings.default_host,
-  :backend_protocol => 'http',
-  :balance_algorithm => 'least_conn',
-  :sort_order => 1,
-  :servers => [
-    {
-      'host' => '127.0.0.1',
-      'port' => 51_000
-    }
-  ],
-  :url_matches => [
-    {
-      :frontend_prefix => '/api-umbrella/',
-      :backend_prefix => '/api/',
-    }
-  ],
-}, :without_protection => true)
-api.save!
-
 user = ApiUser.find_or_initialize_by(:email => 'web.admin.ajax@internal.apiumbrella')
 user.assign_attributes({
   :first_name => 'API Umbrella Admin',
