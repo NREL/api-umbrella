83f245e42c12e524557a00e3e4e4ba4f51e1842d
Nick Muerdter <nick.muerdter@nrel.gov>
2014-10-10 14:26:12 -0600

    Improve logging during tests, so debug logs can be output to a file.
    This is preferable for tests, so we don't have to always worry about
    having quiet output and can better debug things during testing.

--- test/support/env.js
+++ test/support/env.js
@@ -1,4 +1,7 @@
 'use strict';
 
+var path = require('path');
+
 process.env.NODE_ENV = 'test';
-process.env.NODE_LOG_LEVEL = 'error';
+process.env.API_UMBRELLA_LOG_LEVEL = 'debug';
+process.env.API_UMBRELLA_LOG_PATH = path.resolve(__dirname, '../log/test.log');
