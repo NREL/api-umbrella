9cb9dcf96bac31af9fd3edb64dc31614b5de264a
Nick Muerdter <nick.muerdter@nrel.gov>
2014-10-21 06:37:21 -0600

    Fix log processor crashes when invalid url encoded params were passed in

--- test/integration/logging.js
+++ test/integration/logging.js
@@ -68,9 +68,6 @@ describe('logging', function() {
       this.timeout(45000);
 
       var options = _.merge({}, this.options, {
-        qs: {
-          'unique_query_id': this.uniqueQueryId,
-        },
         headers: {
           'Accept': 'text/plain; q=0.5, text/html',
           'Accept-Encoding': 'compress, gzip',
@@ -87,7 +84,8 @@ describe('logging', function() {
         },
       });
 
-      request.get('http://localhost:9080/logging-example/foo/bar/', options, function(error, response) {
+      var requestUrl = 'http://localhost:9080/logging-example/foo/bar/?unique_query_id=' + this.uniqueQueryId + '&url1=http%3A%2F%2Fexample.com%2F%3Ffoo%3Dbar%26foo%3Dbar%20more+stuff&url2=%ED%A1%BC&url3=https%3A//example.com/foo/%D6%D0%B9%FA%BD%AD%CB%D5%CA%A1%B8%D3%D3%DC%CF%D8%D2%BB%C2%A5%C5%CC%CA%C0%BD%F5%BB%AA%B3%C7200%D3%E0%D2%B5%D6%F7%B9%BA%C2%F2%B5%C4%C9%CC%C6%B7%B7%BF%A3%AC%D2%F2%BF%AA%B7%A2%C9%CC%C5%DC%C2%B7%D2%D1%CD%A3%B9%A420%B8%F6%D4%C2%A3%AC%D2%B5%D6%F7%C4%C3%B7%BF%CE%DE%CD%FB%C8%B4%D0%E8%BC%CC%D0%F8%B3%A5%BB%B9%D2%F8%D0%D0%B4%FB%BF%EE%A1%A3%CF%F2%CA%A1%CA%D0%CF%D8%B9%FA%BC%D2%D0%C5%B7%C3%BE%D6%B7%B4%D3%B3%BD%FC2%C4%EA%CE%DE%C8%CB%B4%A6%C0%ED%A1%A3%D4%DA%B4%CB%B0%B8%D6%D0%A3%AC%CE%D2%C3%C7%BB%B3%D2%C9%D3%D0%C8%CB%CA%A7%D6%B0%E4%C2%D6%B0/sites/default/files/googleanalytics/ga.js';
+      request.get(requestUrl, options, function(error, response) {
         response.statusCode.should.eql(200);
 
         waitForLog(this.uniqueQueryId, 44000, function(error, response, hit, record) {
@@ -167,13 +165,20 @@ describe('logging', function() {
           record.request_method.should.eql('GET');
           record.request_origin.should.eql('http://foo.example');
           record.request_path.should.eql('/logging-example/foo/bar/');
-          record.request_query.should.eql({
-            'unique_query_id': this.uniqueQueryId,
-          });
+          Object.keys(record.request_query).sort().should.eql([
+            'unique_query_id',
+            'url1',
+            'url2',
+            'url3',
+          ]);
+          record.request_query.unique_query_id.should.eql(this.uniqueQueryId);
+          record.request_query.url1.should.eql('http://example.com/?foo=bar&foo=bar more stuff');
+          (new Buffer(record.request_query.url2)).toString('base64').should.eql('77+9');
+          (new Buffer(record.request_query.url3)).toString('base64').should.eql('aHR0cHM6Ly9leGFtcGxlLmNvbS9mb28v77+90Lnvv73vv73vv73vv73vv73Koe+/ve+/ve+/ve+/ve+/ve+/vdK7wqXvv73vv73vv73vv73vv73xu6qz77+9MjAw77+977+90rXvv73vv73vv73vv73vv73vv73vv73vv73vv73vv73Gt++/ve+/ve+/ve+/ve+/vfK/qrfvv73vv73vv73vv73vv73Ct++/ve+/vc2j77+977+9MjDvv73vv73vv73Co++/vdK177+977+977+9w7fvv73vv73vv73vv73vv73ItO+/ve+/ve+/ve+/ve+/ve+/ve+/ve+/ve+/ve+/ve+/ve+/ve+/vdC077+977+97qGj77+977+9yqHvv73vv73vv73Yue+/ve+/ve+/ve+/vcW3w77Wt++/vdOz77+977+9Mu+/ve+/ve+/ve+/ve+/vcu077+977+977+977+92rTLsO+/ve+/vdCj77+977+977+977+9x7vvv73vv73vv73vv73vv73vv73vv73Kp9aw77+977+91rAvc2l0ZXMvZGVmYXVsdC9maWxlcy9nb29nbGVhbmFseXRpY3MvZ2EuanM=');
           record.request_referer.should.eql('http://example.com');
           record.request_scheme.should.eql('http');
           (typeof record.request_size).should.eql('number');
-          record.request_url.should.eql('http://localhost:9080/logging-example/foo/bar/?unique_query_id=' + this.uniqueQueryId);
+          record.request_url.should.eql(requestUrl);
           record.request_user_agent.should.eql('curl/7.37.1');
           record.request_user_agent_family.should.eql('cURL');
           record.request_user_agent_type.should.eql('Library');
