d1d14f89d0a6914dac24783c7c277e6a32d95ebf
Nick Muerdter <nick.muerdter@nrel.gov>
2014-11-30 17:47:40 -0700

    Remove defunct redis-convoy related config.

--- test/support/env.js
+++ test/support/env.js
@@ -5,8 +5,3 @@ var path = require('path');
 process.env.NODE_ENV = 'test';
 process.env.API_UMBRELLA_LOG_LEVEL = 'debug';
 process.env.API_UMBRELLA_LOG_PATH = path.resolve(__dirname, '../log/test.log');
-
-// redis-convoy uses the config module and requires a bit more setup when in
-// the test environment (the defaults don't get loaded if NODE_ENV=test.
-process.env.NODE_CONFIG_DIR = path.resolve(__dirname, '../config');
-process.env.NODE_RUNTIME_CONFIG_DIR = process.env.NODE_CONFIG_DIR;
