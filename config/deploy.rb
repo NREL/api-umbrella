e713eebdfcd7f8c480bbde7118eb78fc189a09e6
Nick Muerdter <nick.muerdter@nrel.gov>
2014-12-07 17:31:28 -0700

    Add options to the "reload" command and add new Add "ctl" command.
    The "reload" command can now be used to reload only certain components.
    This is for use during router and web deploys.
    
    The "ctl" command drops you into supervisorctl for easier access.

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,24 +36,12 @@ set :ssh_options, {
 }
 
 namespace :deploy do
-
   desc "Restart application"
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join("tmp/restart.txt")
+    on roles(:app), :in => :sequence, :wait => 5 do
+      execute "api-umbrella" "reload" "--router"
     end
   end
 
   after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, "cache:clear"
-      # end
-    end
-  end
-
 end
