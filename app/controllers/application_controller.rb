554e03dceb408d6557377b14287a54096256acfc
Nick Muerdter <nick.muerdter@nrel.gov>
2015-01-17 10:13:56 -0700

    Fix IE8-IE9 CORS-based API key signup since we switch to Puma.
    There was a difference between how Passenger and Puma handled empty
    Content-Type POST requests that meant the pseudo-CORS requests from
    IE8-9 were no longer working. We're now addressing this more formally
    inside the Rails app, so that we're not dependent on our Rack server
    behavior (so it should work in Puma, Passenger, and any other servers).
    
    Also add tests surrounding these odd content-type situations.
    
    See: https://github.com/18F/api.data.gov/issues/174

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,4 +52,26 @@ class ApplicationController < ActionController::Base
       "#{time.strftime("%b %-d, %Y")} - #{end_of_month.strftime("%b %-d, %Y")}"
     end
   end
+
+  # This allows us to support IE8-9 and their shimmed pseudo-CORS support. This
+  # parses the post body as form data, even if the content-type is text/plain
+  # or unknown.
+  #
+  # The issue is that IE8-9 will send POST data with an empty Content-Type
+  # (see: http://goo.gl/oumNaF). Some Rails servers (Passenger) will treat this
+  # as nil, in which case Rack parses the post data as a form data (see:
+  # http://goo.gl/jEEtCC). However, other Rails server (Puma) will default
+  # an empty Content-Type as "text/plain", in which case Rack will not parse
+  # the post data (see: http://goo.gl/y6JsqL).
+  #
+  # For this latter case of Rack servers, we will force parsing of our post
+  # body as form data so IE's form data is present on the rails "params"
+  # object. But even aside from these differences in Rack servers, this is
+  # probably a good idea, since apparently historically IE8-9 would actually
+  # send the data as "text/plain" rather than an empty content-type.
+  def parse_post_for_pseudo_ie_cors
+    if(request.post? && request.POST.blank? && request.raw_post.present?)
+      params.merge!(Rack::Utils.parse_nested_query(request.raw_post))
+    end
+  end
 end
