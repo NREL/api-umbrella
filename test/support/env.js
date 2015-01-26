a8424cf74b909335df8650d6673a49c1def055ae
Nick Muerdter <nick.muerdter@nrel.gov>
2015-01-25 20:03:28 -0700

    Make sure tests run from the current router dir due to 00d849
    This ensures that the local tests always run from the local directory,
    despite the default config values now using a hard-coded path of
    /opt/api-umbrella/embedded/apps/router/current

--- test/support/env.js
+++ test/support/env.js
@@ -1,6 +1,14 @@
 'use strict';
 
-var path = require('path');
+var fs = require('fs'),
+    path = require('path'),
+    yaml = require('js-yaml');
+
+fs.writeFileSync('/tmp/api-umbrella-test.yml', yaml.safeDump({
+  router: {
+    dir: path.resolve(__dirname, '../../'),
+  },
+}));
 
 process.env.NODE_ENV = 'test';
 process.env.API_UMBRELLA_LOG_LEVEL = 'debug';
