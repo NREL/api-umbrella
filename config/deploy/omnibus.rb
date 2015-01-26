1a20c8853ea7912d5031b011e0e1ca495e3963f4
Nick Muerdter <nick.muerdter@nrel.gov>
2015-01-25 20:18:54 -0700

    Fix omnibus deploys.

--- config/deploy/omnibus.rb
+++ config/deploy/omnibus.rb
@@ -1,3 +1,10 @@
 role :app, ["#{ENV["USER"]}@localhost"]
 role :web, ["#{ENV["USER"]}@localhost"]
 role :db, []
+
+namespace :deploy do
+  task :restart do
+    # There's nothing to reload on a fresh omnibus install, so disable the
+    # restart task.
+  end
+end
