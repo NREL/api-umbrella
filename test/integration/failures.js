634b1aee90ca46d75720b539f11703a054aaccb9
Nick Muerdter <nick.muerdter@nrel.gov>
2015-03-14 17:01:28 -0600

    Fix errors during mongodb replica set re-elections.
    There were two things that might have happened if the primary node in
    a mongodb replica set left:
    
    - The API backends config from MongoDB would get wiped, which might have
      meant that all the API backends disappeared (this happened if the config
      reloader reloaded before the new primary was elected).
    - API key lookups on individual requests may have failed.
    
    This has been fixed in bumps to gatekeeper and api-umbrella-config.
    
    In the router project, we've also increased our mongodb connection
    attempts.
    
    Integration tests have been added using mongo-orchestration to actually
    test how the stack reacts to real-world replicaset changes.

---
+++ test/integration/failures.js
@@ -0,0 +1,144 @@
+'use strict';
+
+require('../test_helper');
+
+var _ = require('lodash'),
+    async = require('async'),
+    execFile = require('child_process').execFile,
+    Factory = require('factory-lady'),
+    request = require('request');
+
+describe('failures', function() {
+  describe('mongodb', function() {
+    before(function fetchMongoDbReplicaSetInfo(done) {
+      // Fetch the replicaset information from the mongo-orchestration.
+      request.get('http://127.0.0.1:13089/v1/replica_sets/test-cluster', function(error, response, body) {
+        response.statusCode.should.eql(200);
+        var data = JSON.parse(body);
+
+        // Put together a replicaset connection argument for passing to the
+        // "--host" arg of the "mongo" command line tool (the syntax is a bit
+        // different from the URL mongo-orchestration returns).
+        this.hostArg = data.id + '/' + _.map(data.members, function(member) {
+          return member.host;
+        }).join(',');
+
+        done();
+      }.bind(this));
+    });
+
+    before(function publishDbConfig(done) {
+      this.timeout(10000);
+
+      // Be sure that these tests interact with a backend published via Mongo,
+      // so we can also catch errors for when the mongo-based configuration
+      // data experiences failures.
+      shared.publishDbConfig({
+        apis: [
+          {
+            _id: 'db-config',
+            frontend_host: 'localhost',
+            backend_host: 'localhost',
+            servers: [
+              {
+                host: '127.0.0.1',
+                port: 9444,
+              }
+            ],
+            url_matches: [
+              {
+                frontend_prefix: '/info/db-config/',
+                backend_prefix: '/info/',
+              }
+            ],
+          },
+        ],
+      }, done);
+    });
+
+    beforeEach(function setOptionDefaults() {
+      this.options = {
+        headers: {
+          'X-Disable-Router-Connection-Limits': 'yes',
+          'X-Disable-Router-Rate-Limits': 'yes',
+        },
+        agentOptions: {
+          maxSockets: 500,
+        },
+      };
+    });
+
+    it('does not drop connections during replicaset elections', function(done) {
+      this.timeout(90000);
+
+      this.apiKeyBatches = {}
+
+      function makeBatch(apiKeys, callback) {
+        async.eachSeries(apiKeys, function(apiKey, next) {
+          request.get('http://localhost:9080/info/db-config/?api_key=' + apiKey, this.options, function(error, response) {
+            should.not.exist(error);
+            response.statusCode.should.eql(200);
+            setTimeout(callback, 10);
+          }.bind(this));
+        }.bind(this), callback);
+      }
+
+      async.series([
+        // Generate batches of unique API keys. Each test will do something to
+        // the replicaset, and then ensure nothing bad happens. Each API key
+        // tested should be unique, so that we're sure we're not relying on any
+        // API key caches. We run a batch of 100 different keys on each change
+        // just to ensure things remain up across all workers for a bit of
+        // time.
+        function(callback) {
+          async.timesSeries(3, function(batchIndex, batchNext) {
+            this.apiKeyBatches[batchIndex] = [];
+
+            async.times(100, function(index, next) {
+              Factory.create('api_user', { settings: { rate_limit_mode: 'unlimited' } }, function(user) {
+                this.apiKeyBatches[batchIndex].push(user.api_key);
+                next();
+              }.bind(this));
+            }.bind(this), batchNext);
+          }.bind(this), callback);
+        }.bind(this),
+
+        // Make an initial batch of requests just to ensure things are working
+        // as expected.
+        function(callback) {
+          makeBatch.call(this, this.apiKeyBatches[0], callback);
+        }.bind(this),
+
+        // Force the primary mongo host to step down for 30 seconds. (We do
+        // this via the command line instead of mongo-orchestration's API,
+        // since mongo-orchestration forces a 60 second step down period, and
+        // we want something shorter).
+        function(callback) {
+          execFile('mongo', ['--host', this.hostArg, '--eval', 'rs.stepDown(30)'], function(error) {
+            callback();
+          });
+        }.bind(this),
+
+        // Run another batch of requests with the previous primary stepped
+        // down. Requests may be returned more slowly while things wait for the
+        // replicaset election to kick in, but no connections should fail.
+        function(callback) {
+          makeBatch.call(this, this.apiKeyBatches[1], callback);
+        }.bind(this),
+
+        // Force the new primary server offline completely. Just another sanity
+        // check that a true failure and election will be handled properly.
+        function(callback) {
+          execFile('mongo', ['--host', this.hostArg, '--eval', 'db.shutdownServer({ force: true })', 'admin'], function(error) {
+            callback(error);
+          });
+        }.bind(this),
+
+        // Run the next batch of requests that should succeed.
+        function(callback) {
+          makeBatch.call(this, this.apiKeyBatches[2], callback);
+        }.bind(this),
+      ], done);
+    });
+  });
+});
