a67dcbb3d9c5080de7c644ac2070c349bed083be
CM Lubinski <christopher.lubinski@gsa.gov>
2015-05-11 18:44:35 +0000

    UI for admins now includes CSV link

--- app/assets/javascripts/admin/views/admins/table_view.js
+++ app/assets/javascripts/admin/views/admins/table_view.js
@@ -4,7 +4,7 @@ Admin.AdminsTableView = Ember.View.extend({
   classNames: ['table', 'table-striped', 'table-bordered', 'table-condensed'],
 
   didInsertElement: function() {
-    this.$().DataTable({
+    var dataTable = this.$().DataTable({
       serverSide: true,
       ajax: '/api-umbrella/v1/admins.json',
       pageLength: 50,
@@ -12,6 +12,7 @@ Admin.AdminsTableView = Ember.View.extend({
       columns: [
         {
           data: 'username',
+          name: 'Username',
           title: 'Username',
           defaultContent: '-',
           render: _.bind(function(email, type, data) {
@@ -25,24 +26,28 @@ Admin.AdminsTableView = Ember.View.extend({
         },
         {
           data: 'email',
+          name: 'E-mail',
           title: 'E-mail',
           defaultContent: '-',
           render: Admin.DataTablesHelpers.renderEscaped,
         },
         {
           data: 'name',
+          name: 'Name',
           title: 'Name',
           defaultContent: '-',
           render: Admin.DataTablesHelpers.renderEscaped,
         },
         {
           data: 'group_names',
+          name: 'Groups',
           title: 'Groups',
           render: Admin.DataTablesHelpers.renderListEscaped,
         },
         {
           data: 'last_sign_in_at',
           type: 'date',
+          name: 'Last Signed In',
           title: 'Last Signed In',
           defaultContent: '-',
           render: Admin.DataTablesHelpers.renderTime,
@@ -50,11 +55,15 @@ Admin.AdminsTableView = Ember.View.extend({
         {
           data: 'created_at',
           type: 'date',
+          name: 'Created',
           title: 'Created',
           defaultContent: '-',
           render: Admin.DataTablesHelpers.renderTime,
         }
       ]
-    });
+    })
+    dataTable.on('draw.dt', function() {
+      this.get('controller').send('paramsChange', dataTable.ajax.params());
+    }.bind(this));
   },
 });
