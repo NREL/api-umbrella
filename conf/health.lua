1516b367106aa6a8f351b6eebcc4f75e8051187a
Nick Muerdter <nick.muerdter@nrel.gov>
2015-05-12 19:15:17 -0600

    Fix erroneous passing health check when config fetching failed.
    If elasticsearch was up, but the config fetching from Mongo had failed,
    we were erroneously returning a "green" status.
    
    Also provide more details on the breakdown of the health status to
    better indicate what pieces are functional or not.

--- conf/health.lua
+++ conf/health.lua
@@ -3,10 +3,19 @@ local inspect = require "inspect"
 
 local response = {
   status = "red",
+  details = {
+    apis_config = "red",
+    api_users = "red",
+    analytics_db = "red",
+  },
 }
 
-if ngx.shared.apis:get("last_fetched_at") and ngx.shared.api_users:get("last_fetched_at") then
-  response["status"] = "yellow"
+if ngx.shared.apis:get("last_fetched_at") then
+  response["details"]["apis_config"] = "green"
+end
+
+if ngx.shared.api_users:get("last_fetched_at") then
+  response["details"]["api_users"] = "green"
 end
 
 local http = require "resty.http"
@@ -15,9 +24,13 @@ local httpc = http.new()
 local res, err = httpc:request_uri(config["elasticsearch"]["hosts"][1] .. "/_cluster/health")
 if not err and res.body then
   local elasticsearch_health = cjson.decode(res.body)
-  if elasticsearch_health["status"] == "green" then
-    response["status"] = "green"
-  end
+  response["details"]["analytics_db"] = elasticsearch_health["status"]
+end
+
+if response["details"]["apis_config"] == "green" and response["details"]["api_users"] == "green" and response["details"]["analytics_db"] == "green" then
+  response["status"] = "green"
+elseif response["details"]["apis_config"] == "green" and response["details"]["api_users"] == "green" then
+  response["status"] = "yellow"
 end
 
 ngx.say(cjson.encode(response))
