4773da852068713a15d1668d55c6f7e0e681c6f6
Nick Muerdter <nick.muerdter@nrel.gov>
2015-05-16 10:02:55 -0600

    Allow the asset precompiling to be skipped in the test environment.
    This is for the router component's integration tests, where even though
    we're running in the app in the test environment, we don't actually want
    to test the internals of the web component, like asset precompilation
    (we'll leave that for only when running the web tests directly).

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,27 +32,29 @@ ApiUmbrella::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
-  # Use precompiled assets in test mode so we can properly catch errors
-  # triggered by not having assets in the precompile list.
-  config.assets.compile = false
-  config.assets.digest = true
-  config.assets.prefix = "/test-assets"
+  if(ENV["PRECOMPILE_TEST_ASSETS"].to_s != "false")
+    # Use precompiled assets in test mode so we can properly catch errors
+    # triggered by not having assets in the precompile list.
+    config.assets.compile = false
+    config.assets.digest = true
+    config.assets.prefix = "/test-assets"
 
-  # Precompile additional assets for the test environment.
-  config.assets.precompile += %w(
-    admin_test.css
-    admin_test.js
-  )
+    # Precompile additional assets for the test environment.
+    config.assets.precompile += %w(
+      admin_test.css
+      admin_test.js
+    )
 
-  config.before_initialize do |app|
-    # Run the asset precompile phase prior to running tests so that we can test
-    # with precompiled assets (so we're more properly testing for any potential
-    # missing precompiled assets). However, note that we have to call this
-    # here, instead of in something like an rspec before suite so that the
-    # precompile happens early enough so that the current environment can load
-    # the precompiled digests.
-    unless ENV["PRECOMPILE_TEST_ASSETS"]
-      system("RAILS_ENV=test PRECOMPILE_TEST_ASSETS=true bundle exec rake assets:precompile")
+    config.before_initialize do |app|
+      # Run the asset precompile phase prior to running tests so that we can test
+      # with precompiled assets (so we're more properly testing for any potential
+      # missing precompiled assets). However, note that we have to call this
+      # here, instead of in something like an rspec before suite so that the
+      # precompile happens early enough so that the current environment can load
+      # the precompiled digests.
+      unless ENV["PRECOMPILE_TEST_ASSETS"]
+        system("RAILS_ENV=test PRECOMPILE_TEST_ASSETS=true bundle exec rake assets:precompile")
+      end
     end
   end
 end
