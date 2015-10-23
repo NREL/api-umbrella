4339e926dbcce0702df96f87bbaa9c08b902a50e
Nick Muerdter <nick.muerdter@nrel.gov>
2015-10-23 11:27:49 -0600

    Add some comments surrounding the error handler.

--- src/api-umbrella/proxy/error_handler.lua
+++ src/api-umbrella/proxy/error_handler.lua
@@ -1,4 +1,4 @@
-local is_array = require "api-umbrella.utils.is_array"
+local is_hash = require "api-umbrella.utils.is_hash"
 local deep_merge_overwrite_arrays = require "api-umbrella.utils.deep_merge_overwrite_arrays"
 local lustache = require "lustache"
 local mustache_unescape = require "api-umbrella.utils.mustache_unescape"
@@ -84,7 +84,7 @@ local function render_template(template, data, format, strip_whitespace)
     return nil, "template error"
   end
 
-  if not data or type(data) ~= "table" or is_array(data) then
+  if not is_hash(data) then
     ngx.log(ngx.ERR, "render_template passed invalid data (not a table)")
     return nil, "template error"
   end
@@ -129,68 +129,102 @@ return function(denied_code, settings, extra_data)
     settings = config["apiSettings"]
   end
 
+  -- Try to determine the format of the request (JSON, XML, etc), so we can
+  -- attempt to match our response to the expected format.
   local format, content_type = request_format()
 
+  -- Fetch "common" error data variables, for variables like "contact_url" that
+  -- might be in use across all error messages.
   local common_data = deepcopy(settings["error_data"]["common"])
-  if not common_data or type(common_data) ~= "table" or is_array(common_data) then
+  if not is_hash(common_data) then
+    -- Fallback to the built-in default data that isn't subject to any
+    -- API-specific overrides (so it should always be a valid hash).
     common_data = deepcopy(config["apiSettings"]["error_data"]["common"])
   end
 
+  -- Fetch the error data specific to this error message (over rate limit, key
+  -- missing, etc).
   local error_data = deepcopy(settings["error_data"][denied_code])
-  if not error_data or type(error_data) ~= "table" or is_array(error_data) then
+  if not is_hash(error_data) then
     error_data = deepcopy(settings["error_data"]["internal_server_error"])
-
-    -- Fallback to the built-in default template that isn't subject to any
-    -- API-specific overrides.
-    if not error_data or type(error_data) ~= "table" or is_array(error_data) then
+    if not is_hash(error_data) then
+      -- Fallback to the built-in default data that isn't subject to any
+      -- API-specific overrides (so it should always be a valid hash).
       error_data = deepcopy(config["apiSettings"]["error_data"]["internal_server_error"])
     end
   end
 
+  -- Begin building the combined data available to the error template, starting
+  -- with the base_url (based on the current URL being hit).
   local data = { base_url = utils.base_url() }
-  deep_merge_overwrite_arrays(data, common_data)
 
   -- Support legacy camel-case capitalization of variables. Moving forward,
   -- we're trying to clean things up and standardize on snake_case.
-  if not data["baseUrl"] then
-    data["baseUrl"] = data["base_url"]
-  end
+  data["baseUrl"] = data["base_url"]
+
+  -- Later we need to loop through the data table roughly in order of
+  -- insertion. Since tables have no order, keep track of things separately so
+  -- we can loop through the logical groups in order.
+  local data_ordered_keys = keys(data)
+
+  -- Add the common data.
+  deep_merge_overwrite_arrays(data, common_data)
+  append_array(data_ordered_keys, keys(common_data))
+
+  -- Support legacy camel-case capitalization of variables. Moving forward,
+  -- we're trying to clean things up and standardize on snake_case.
   if not data["signupUrl"] then
     data["signupUrl"] = data["signup_url"]
+    table.insert(data_ordered_keys, "signupUrl")
   end
   if not data["contactUrl"] then
     data["contactUrl"] = data["contact_url"]
+    table.insert(data_ordered_keys, "contactUrl")
   end
 
-  local data_ordered_keys = keys(data)
-
+  -- Add the error-specific data.
   deep_merge_overwrite_arrays(data, error_data)
   append_array(data_ordered_keys, keys(error_data))
 
+  -- Add any extra data we might be passing internally.
   if extra_data then
     deep_merge_overwrite_arrays(data, extra_data)
     append_array(data_ordered_keys, keys(extra_data))
   end
 
+  -- Loop through all the data variables we have, treating each variable as a
+  -- potential template. This allows for variables to contain other variables.
+  -- This allows for variables to contain others (like contact_url defaulting
+  -- to "{{base_url}}/contact/"), as well as the error message variable to
+  -- contain variables like "Contact us at {{contact_url}}".
+  --
+  -- We have to pay attention to the order with which we loop through things,
+  -- because we have a bit of a circular issue with the example above of
+  -- "message" containing "{{contact_url}}" and "contact_url" containing
+  -- "{{base_url}}". We solve this by trying to loop through things in a
+  -- logical order of how our variables typically build on each other (base_url
+  -- first, followed by common_data, followed by the error_data). This isn't
+  -- perfect, but saves us from multiple passes. But perhaps we should rethink
+  -- this and disallow nested variables.
   for _, key in ipairs(data_ordered_keys) do
     if data[key] and type(data[key]) == "string" then
       data[key] = render_template(data[key], data)
     end
   end
 
-  local status_code = data["status_code"]
-  if not status_code then
-    status_code = 500
-  end
+  -- Determine the status code for the HTTP response.
+  local status_code = data["status_code"] or 500
 
+  -- Fetch the template for rendering the different formats of error messages
+  -- (JSON, XML, etc).
   local template = settings["error_templates"][format]
-
-  -- Fallback to the built-in default data that isn't subject to any
-  -- API-specific overrides.
   if not template or type(template) ~= "string" then
+    -- Fallback to the built-in default template that isn't subject to any
+    -- API-specific overrides (so it should always be a valid template).
     template = config["apiSettings"]["error_templates"][format]
   end
 
+  -- Render the error message, substituting the variables.
   local output, template_err = render_template(template, data, format, true)
 
   -- Allow all errors to be loaded over CORS (in case any underlying APIs are
