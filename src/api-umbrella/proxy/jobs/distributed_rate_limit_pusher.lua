0a24560291d9a6720d6bab52fc57bd2db791aad4
Nick Muerdter <nick.muerdter@nrel.gov>
2015-11-24 09:51:55 -0700

    Fix the distributed rate limit pusher not firing as often as needed.
    This was causing some test failures, since each worker process was not
    syncing its local data as frequently as expected. The rate limit pusher
    is a bit of an odd case amongst our background workers, since it's the
    one place where we don't actually want a mutex or locking of any
    fashion. So we've reverted its timer setup to the previous setup that
    doesn't use the new interval_lock utility, but adding a comment about
    why this is done differently.

--- src/api-umbrella/proxy/jobs/distributed_rate_limit_pusher.lua
+++ src/api-umbrella/proxy/jobs/distributed_rate_limit_pusher.lua
@@ -1,13 +1,13 @@
 local _M = {}
 
 local distributed_rate_limit_queue = require "api-umbrella.proxy.distributed_rate_limit_queue"
-local interval_lock = require "api-umbrella.utils.interval_lock"
 local mongo = require "api-umbrella.utils.mongo"
 local types = require "pl.types"
 
 local is_empty = types.is_empty
 
 local delay = 0.25  -- in seconds
+local new_timer = ngx.timer.at
 
 local indexes_created = false
 
@@ -78,8 +78,42 @@ local function do_check()
   end
 end
 
+-- Repeat calls to do_check() inside each worker on the specified interval
+-- (every 0.25 seconds).
+--
+-- We don't use interval_lock.repeat_with_mutex() here like most of our other
+-- background jobs, because in this job's case we're pushing local worker data
+-- into the database. In this case, we don't want a mutex across workers, since
+-- we want each worker to operate independently and fire every 0.25 seconds to
+-- push it's local data to the database. With a mutex, certain workers may not
+-- be called for longer periods of time causing the local data to build up and
+-- not be synced as frequently as we expect.
+local function check(premature)
+  if premature then
+    return
+  end
+
+  local ok, err = pcall(do_check)
+  if not ok then
+    ngx.log(ngx.ERR, "failed to run backend load cycle: ", err)
+  end
+
+  ok, err = new_timer(delay, check)
+  if not ok then
+    if err ~= "process exiting" then
+      ngx.log(ngx.ERR, "failed to create timer: ", err)
+    end
+
+    return
+  end
+end
+
 function _M.spawn()
-  interval_lock.repeat_with_mutex('distributed_rate_limit_pusher', delay, do_check)
+  local ok, err = new_timer(0, check)
+  if not ok then
+    ngx.log(ngx.ERR, "failed to create timer: ", err)
+    return
+  end
 end
 
 return _M
