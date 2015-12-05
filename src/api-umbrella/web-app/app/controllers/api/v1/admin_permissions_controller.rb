7170d65f95cd45f85b0ceaa497916c7b7600d0f3
Nick Muerdter <nick.muerdter@nrel.gov>
2015-12-04 18:39:08 -0700

    Fix admin permissions API endpoint and add test.
    I'm not exactly sure what's going on, but this seems like it must have
    broken during some gem updates as part of the lua upgrade. The
    respond_with output is doing some odd things and the structure of the
    data being returned now isn't what's expected.

--- src/api-umbrella/web-app/app/controllers/api/v1/admin_permissions_controller.rb
+++ src/api-umbrella/web-app/app/controllers/api/v1/admin_permissions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::AdminPermissionsController < Api::V1::BaseController
   skip_after_filter :verify_authorized, :only => [:index]
 
   def index
-    @admin_permissions = AdminPermission.sorted.all
+    @admin_permissions = AdminPermission.sorted.all.to_a
     respond_with(:api_v1, @admin_permissions, :root => "admin_permissions")
   end
 end
