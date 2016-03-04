be45c9d41e0ff328f7f6582a72e094d6b2125c5a
Nick Muerdter <nick.muerdter@nrel.gov>
2016-03-03 20:24:20 -0700

    A rough, first pass at falling back to Presto SQL queries.
    This is to answer queries that cannot be satisfied by the Kylin data
    cubes.

--- src/api-umbrella/web-app/app/models/log_search_sql.rb
+++ src/api-umbrella/web-app/app/models/log_search_sql.rb
@@ -86,59 +86,126 @@ class LogSearchSql
     @result_processors = []
   end
 
+  def build_query(query)
+    select = @query[:select] + (query[:select] || [])
+    sql = "SELECT #{select.join(", ")} FROM api_umbrella.logs"
+
+    where = @query[:where] + (query[:where] || [])
+    if(where.present?)
+      sql << " WHERE #{where.map { |clause| "(#{clause})" }.join(" AND ")}"
+    end
+
+    group_by = @query[:group_by] + (query[:group_by] || [])
+    if(group_by.present?)
+      sql << " GROUP BY #{group_by.join(", ")}"
+    end
+
+    order_by = @query[:order_by] + (query[:order_by] || [])
+    if(order_by.present?)
+      sql << " ORDER BY #{order_by.join(", ")}"
+    end
+
+    limit = query[:limit] || @query[:limit]
+    if(limit.present?)
+      sql << " LIMIT #{limit}"
+    end
+
+    offset = query[:offset] || @query[:offset]
+    if(offset.present?)
+      sql << " OFFSET #{offset}"
+    end
+
+    sql
+  end
+
+  def execute_kylin(sql)
+    @kylin_conn ||= Faraday.new(:url => "http://kylin.host") do |faraday|
+      faraday.response :logger
+      faraday.adapter Faraday.default_adapter
+      faraday.basic_auth "ADMIN", "KYLIN"
+    end
+
+    Rails.logger.info(sql)
+    response = @kylin_conn.post do |req|
+      req.url "/kylin/api/query"
+      req.headers["Content-Type"] = "application/json"
+      req.body = MultiJson.dump({
+        :acceptPartial => false,
+        :project => "api_umbrella",
+        :sql => sql,
+      })
+    end
+
+    if(response.status != 200)
+      Rails.logger.error(response.body)
+      raise "Kylin Error"
+    end
+
+    MultiJson.load(response.body)
+  end
+
+  def execute_presto(sql)
+    @presto_conn ||= Faraday.new(:url => "http://presto.host") do |faraday|
+      faraday.response :logger
+      faraday.adapter Faraday.default_adapter
+    end
+
+    Rails.logger.info(sql)
+    response = @presto_conn.post do |req|
+      req.url "/v1/statement"
+      req.headers["Content-Type"] = "text/plain"
+      req.headers["X-Presto-User"] = "presto"
+      req.headers["X-Presto-Catalog"] = "hive"
+      req.headers["X-Presto-Schema"] = "default"
+      req.body = sql
+    end
+
+    results = {
+      "columnMetas" => [],
+      "results" => [],
+    }
+    while(response.status == 200)
+      query_result = MultiJson.load(response.body)
+      if(results["columnMetas"].empty? && query_result["columns"])
+        results["columnMetas"] = query_result["columns"].map do |column|
+          {
+            "label" => column["name"],
+          }
+        end
+      end
+
+      if(query_result["data"].present?)
+        results["results"] += query_result["data"]
+      end
+
+      if(query_result["nextUri"])
+        response = @presto_conn.get do |req|
+          req.url(query_result["nextUri"])
+        end
+      else
+        break
+      end
+    end
+
+    if(response.status != 200)
+      Rails.logger.error(response.body)
+      raise "Presto Error"
+    end
+
+    results
+  end
+
   def execute_query(query_name, query = {})
     unless @query_results[query_name]
-      select = @query[:select] + (query[:select] || [])
-      sql = "SELECT #{select.join(", ")} FROM api_umbrella.logs"
+      sql = build_query(query)
 
-      where = @query[:where] + (query[:where] || [])
-      if(where.present?)
-        sql << " WHERE #{where.map { |clause| "(#{clause})" }.join(" AND ")}"
+      begin
+        results = execute_kylin(sql)
+      rescue
+        results = execute_presto(sql)
       end
 
-      group_by = @query[:group_by] + (query[:group_by] || [])
-      if(group_by.present?)
-        sql << " GROUP BY #{group_by.join(", ")}"
-      end
-
-      order_by = @query[:order_by] + (query[:order_by] || [])
-      if(order_by.present?)
-        sql << " ORDER BY #{order_by.join(", ")}"
-      end
-
-      limit = query[:limit] || @query[:limit]
-      if(limit.present?)
-        sql << " LIMIT #{limit}"
-      end
-
-      offset = query[:offset] || @query[:offset]
-      if(offset.present?)
-        sql << " OFFSET #{offset}"
-      end
-
-      conn = Faraday.new(:url => "http://kylin.host") do |faraday|
-        faraday.response :logger
-        faraday.adapter Faraday.default_adapter
-        faraday.basic_auth "ADMIN", "KYLIN"
-      end
-
-      Rails.logger.info(sql)
-      response = conn.post do |req|
-        req.url "/kylin/api/query"
-        req.headers["Content-Type"] = "application/json"
-        req.body = MultiJson.dump({
-          :acceptPartial => false,
-          :project => "api_umbrella",
-          :sql => sql,
-        })
-      end
-
-      if(response.status != 200)
-        Rails.logger.error(response.body)
-        raise "Error"
-      end
-
-      @query_results[query_name] = MultiJson.load(response.body)
+      @query_results[query_name] = results
     end
 
     @query_results[query_name]
@@ -146,7 +213,7 @@ class LogSearchSql
 
   def result
     if(@query_results.empty? || @queries[:default])
-      execute_query(:default, @queries[:default])
+      execute_query(:default, @queries[:default] || {})
     end
 
     @result = LogResultSql.new(self, @query_results)
@@ -215,7 +282,8 @@ class LogSearchSql
         value = rule["value"]
 
         if(!CASE_SENSITIVE_FIELDS.include?(rule["field"]) && value.kind_of?(String))
-          value.downcase!
+          # FIXME: Is this needed now that everything is case-sensitive in SQL?
+          # value.downcase!
         end
 
         if(value.present?)
@@ -308,7 +376,7 @@ class LogSearchSql
   end
 
   def filter_by_date_range!
-    @query[:where] << @sequel.literal(Sequel.lit("request_at_date >= :start_time_date AND request_at_date <= :end_time_date", {
+    @query[:where] << @sequel.literal(Sequel.lit("request_at_date >= CAST(:start_time_date AS DATE) AND request_at_date <= CAST(:end_time_date AS DATE)", {
       :start_time_date => @start_time.strftime("%Y-%m-%d"),
       :end_time_date => @end_time.strftime("%Y-%m-%d"),
     }))
