b8754313cff7030b870282c6ba472f52081e98d9
Nick Muerdter <nick.muerdter@nrel.gov>
2016-04-03 12:40:53 -0600

    Add "make outdated" command for identifying outdated dependencies.
    This is based on the rake task we used to have in the old omnibus build
    (https://github.com/NREL/omnibus-api-umbrella/blob/master/Rakefile), but
    updated for our new software stack.
    
    This is a handy way to check our dependency versions to see what might
    need to be updated.

---
+++ build/cmake/outdated.cmake
@@ -0,0 +1,14 @@
+add_custom_command(
+  OUTPUT ${CMAKE_SOURCE_DIR}/build/scripts/vendor/bundle
+  DEPENDS ${CMAKE_SOURCE_DIR}/build/scripts/Gemfile ${CMAKE_SOURCE_DIR}/build/scripts/Gemfile.lock
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/build/scripts
+  COMMAND bundle install --clean --path=${CMAKE_SOURCE_DIR}/build/scripts/vendor/bundle
+    COMMAND touch ${CMAKE_SOURCE_DIR}/build/scripts/vendor/bundle
+)
+
+add_custom_target(
+  outdated
+  DEPENDS ${CMAKE_SOURCE_DIR}/build/scripts/vendor/bundle
+  WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/build/scripts
+  COMMAND ${CMAKE_SOURCE_DIR}/build/scripts/outdated
+)
