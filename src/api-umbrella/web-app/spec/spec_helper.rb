a4bb730eb7af49669a318e10c0add7dafc972dec
Nick Muerdter <nick.muerdter@nrel.gov>
2016-04-10 10:42:53 -0600

    Remove coveralls, since it hasn't been setup in our CI env for a while.

--- src/api-umbrella/web-app/spec/spec_helper.rb
+++ src/api-umbrella/web-app/spec/spec_helper.rb
@@ -1,6 +1,3 @@
-require 'coveralls'
-Coveralls.wear!
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
