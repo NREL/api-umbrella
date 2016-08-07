9d1a8c1c046d02b91a2690c1762908a356c21bd7
Nick Muerdter <nick.muerdter@nrel.gov>
2016-08-07 15:20:12 -0600

    Clarify variable names.

--- src/api-umbrella/admin-ui/app/mixins/confirmation.js
+++ src/api-umbrella/admin-ui/app/mixins/confirmation.js
@@ -8,26 +8,26 @@ export default Ember.Mixin.create(ConfirmationMixin, {
     // rely on ember-data's builtin dirty tracking, since it considers all new
     // records dirty and also doesn't currently support nested/embedded models:
     // https://github.com/emberjs/rfcs/pull/21
-    model.set('_confirmationInitialSerialized', model.serialize());
+    model.set('_confirmationRecordInitialSerialized', model.serialize());
 
     // Determine when the record gets saved, since we don't want to prompt
     // about navigating away if we're in the process of saving the record.
-    model.set('_confirmationSaved', false);
+    model.set('_confirmationRecordIsSaved', false);
     model.on('didCreate', function() {
-      model.set('_confirmationSaved', true);
+      model.set('_confirmationRecordIsSaved', true);
     });
     model.on('didUpdate', function() {
-      model.set('_confirmationSaved', true);
+      model.set('_confirmationRecordIsSaved', true);
     });
   },
 
   isPageDirty(model) {
     if(model) {
-      let saved = model.get('_confirmationSaved');
+      let saved = model.get('_confirmationRecordIsSaved');
       if(saved) {
         return false;
       } else {
-        let initialSerialized = model.get('_confirmationInitialSerialized');
+        let initialSerialized = model.get('_confirmationRecordInitialSerialized');
         let currentSerialized = model.serialize();
         return !_.isEqual(currentSerialized, initialSerialized);
       }
