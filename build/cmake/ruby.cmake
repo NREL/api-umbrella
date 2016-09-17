03b1dc0b8e0b8601fb4b026c57285356ad405b6d
Nick Muerdter <nick.muerdter@nrel.gov>
2016-09-17 16:42:14 -0600

    Use consistent and newer version of CMake to simplify some things.
    Download and use CMake 3.6.2 for the build process. This provides some
    nice features, like DOWNLOAD_NO_EXTRACT to simplify downloading non
    zipped archives. This allows us to more easily checksum the bundler and
    luarock dependencies. Using a newer version also allows us to remove a
    few workarounds for older versions (like .xz support being broken). We
    can also remove the Ubuntu 12.04 workarounds for the CI environment,
    since all environments are now using the newer version.

--- build/cmake/ruby.cmake
+++ build/cmake/ruby.cmake
@@ -11,9 +11,11 @@ ExternalProject_Add(
 ExternalProject_Add(
   bundler
   DEPENDS ruby
-  DOWNLOAD_COMMAND cd <SOURCE_DIR> && curl -OL https://rubygems.org/downloads/bundler-${BUNDLER_VERSION}.gem
+  URL https://rubygems.org/downloads/bundler-${BUNDLER_VERSION}.gem
+  URL_HASH SHA256=${BUNDLER_HASH}
+  DOWNLOAD_NO_EXTRACT 1
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND env PATH=${STAGE_EMBEDDED_DIR}/bin:$ENV{PATH} gem uninstall bundler --all --executables
-    COMMAND env PATH=${STAGE_EMBEDDED_DIR}/bin:$ENV{PATH} gem install <SOURCE_DIR>/bundler-${BUNDLER_VERSION}.gem --no-rdoc --no-ri --env-shebang --local
+    COMMAND env PATH=${STAGE_EMBEDDED_DIR}/bin:$ENV{PATH} gem install <DOWNLOADED_FILE> --no-rdoc --no-ri --env-shebang --local
 )
