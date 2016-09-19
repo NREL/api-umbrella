7afa97caeafbc0f647200c9ddb134d247021fd6c
Nick Muerdter <nick.muerdter@nrel.gov>
2016-09-19 16:14:24 -0600

    Get more admin-ui integration tests running against upgraded admin.

---
+++ test/ruby/admin-ui/test_elasticsearch_proxy.rb
@@ -0,0 +1,61 @@
+require "test_helper"
+
+class TestAdminUiElasticsearchProxy < Minitest::Capybara::Test
+  include Capybara::Screenshot::MiniTestPlugin
+  include ApiUmbrellaTests::AdminAuth
+  include ApiUmbrellaTests::Setup
+
+  def setup
+    setup_server
+  end
+
+  def test_redirect_to_login_for_unauthenticated_requests
+    visit "/admin/elasticsearch"
+    assert_content("You need to sign in")
+    refute_content('"lucene_version"')
+    assert_match(%r{/admin/login\z}, page.current_url)
+
+    visit "/admin/elasticsearch/_search"
+    assert_content("You need to sign in")
+    refute_content('"hits"')
+    assert_match(%r{/admin/login\z}, page.current_url)
+  end
+
+  def test_forbidden_for_unauthorized_admins
+    admin_login(FactoryGirl.create(:limited_admin))
+
+    visit "/admin/elasticsearch"
+    assert_equal(403, page.status_code)
+    assert_content("Forbidden")
+    refute_content('"lucene_version"')
+
+    visit "/admin/elasticsearch/_search"
+    assert_equal(403, page.status_code)
+    assert_content("Forbidden")
+    refute_content('"hits"')
+  end
+
+  def test_allowed_for_superuser_admins
+    admin_login
+
+    visit "/admin/elasticsearch"
+    assert_equal(200, page.status_code)
+    assert_content('"lucene_version"')
+
+    visit "/admin/elasticsearch/_search"
+    assert_equal(200, page.status_code)
+    assert_content('"hits"')
+
+    # Redirect rewriting
+    response = Typhoeus.get("https://127.0.0.1:9081/admin/elasticsearch/_plugin/foobar", {
+      :ssl_verifypeer => false,
+      :headers => {
+        "Cookie" => "_api_umbrella_session=#{page.driver.cookies["_api_umbrella_session"].value}",
+      },
+    })
+    assert_equal(301, response.code)
+    assert_equal("/admin/elasticsearch/_plugin/foobar/", response.headers["Location"])
+    assert_match(%r{URL=/admin/elasticsearch/_plugin/foobar/>}, response.body)
+    assert_equal(response.body.bytesize, response.headers["Content-Length"].to_i)
+  end
+end
---
+++ test/ruby/admin-ui/test_roles.rb
@@ -0,0 +1,93 @@
+require "test_helper"
+
+class TestAdminUiElasticsearchProxy < Minitest::Capybara::Test
+  include Capybara::Screenshot::MiniTestPlugin
+  include ApiUmbrellaTests::AdminAuth
+  include ApiUmbrellaTests::Setup
+
+  def setup
+    setup_server
+  end
+
+  def test_prefills_user_roles
+    FactoryGirl.create(:api_user, :roles => ["test-user-role"])
+    admin_login
+    visit "/admin/#/api_users/new"
+    wait_for_ajax
+
+    find(".selectize-input").click
+    assert_text("test-user-role")
+  end
+
+  def test_prefills_api_roles
+    FactoryGirl.create(:api, :settings => { :required_roles => ["test-api-role"] })
+    admin_login
+    visit "/admin/#/api_users/new"
+    wait_for_ajax
+
+    find(".selectize-input").click
+    assert_text("test-api-role")
+  end
+
+  def test_refresh_prefilled_options_added_during_current_session
+    admin_login
+    visit "/admin/#/api_users/new"
+    wait_for_ajax
+
+    fill_in "E-mail", :with => "example@example.com"
+    fill_in "First Name", :with => "John"
+    fill_in "Last Name", :with => "Doe"
+    check "User agrees to the terms and conditions"
+    find(".selectize-input input").set("test-new-role")
+    find(".selectize-dropdown-content div", :text => /Add test-new-role/).click
+    click_button("Save")
+
+    assert_text("Successfully saved the user")
+    page.execute_script("PNotify.removeAll()")
+    refute_text("Successfully saved the user")
+
+    click_link("Add New API User")
+    wait_for_ajax
+    find(".selectize-input").click
+    assert_text("test-new-role")
+  end
+
+  def test_share_roles_between_users_and_apis_forms
+    admin_login
+    visit "/admin/#/api_users/new"
+    wait_for_ajax
+
+    fill_in "E-mail", :with => "example@example.com"
+    fill_in "First Name", :with => "John"
+    fill_in "Last Name", :with => "Doe"
+    check "User agrees to the terms and conditions"
+    find(".selectize-input input").set("test-new-user-role")
+    find(".selectize-dropdown-content div", :text => /Add test-new-user-role/).click
+    click_button("Save")
+
+    assert_content("Successfully saved the user")
+
+    visit "/admin/#/apis/new"
+    wait_for_ajax
+
+    find("a", :text => /Global Request Settings/).click
+    find(".selectize-input").click
+    assert_content("test-new-user-role")
+
+    find("a", :text => /Sub-URL Request Settings/).click
+    find("button", :text => /Add URL Settings/).click
+    find(".modal .selectize-input").click
+    assert_content("test-new-user-role")
+  end
+
+  private
+
+  def wait_for_ajax
+    Timeout.timeout(Capybara.default_max_wait_time) do
+      while(page.evaluate_script("jQuery.active") > 0)
+        sleep 0.1
+      end
+    end
+    refute_content(".busy-blocker")
+  end
+end
