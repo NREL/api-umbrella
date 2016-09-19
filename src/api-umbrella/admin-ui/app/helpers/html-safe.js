9ef1003dc51935f45fb428daaced838718bc08b4
Nick Muerdter <nick.muerdter@nrel.gov>
2016-09-19 15:53:50 -0600

    Fix HTML safety deprecation.

--- src/api-umbrella/admin-ui/app/helpers/html-safe.js
+++ src/api-umbrella/admin-ui/app/helpers/html-safe.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function htmlSafe(params) {
   let value = params[0];
-  return new Ember.Handlebars.SafeString(value);
+  return new Ember.String.htmlSafe(value);
 }
 
 export default Ember.Helper.helper(htmlSafe);
