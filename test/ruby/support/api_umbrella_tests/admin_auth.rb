b24828fd12599d32b42c54d428b08abaecf8026d
Nick Muerdter <nick.muerdter@nrel.gov>
2016-09-19 16:05:48 -0600

    Fix intermittent ajax test failures due to canceled ajax requests.

--- test/ruby/support/api_umbrella_tests/admin_auth.rb
+++ test/ruby/support/api_umbrella_tests/admin_auth.rb
@@ -8,6 +8,12 @@ module ApiUmbrellaTests
       visit "/admins/auth/developer"
       fill_in "Email:", :with => admin.username
       click_button "Sign In"
+
+      # Wait for the page to fully load, including the /admin/auth ajax request
+      # which will fill out the "My Account" link. If we don't wait, then
+      # navigating to another page immediately may cancel the previous
+      # /admin/auth ajax request if it hadn't finished throwing some errors.
+      assert_link("My Account", :href => /#{admin.id}/, :visible => :all)
     end
   end
 end
