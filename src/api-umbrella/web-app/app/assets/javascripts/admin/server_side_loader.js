e1c393adbe0355aa7a65499dd953afb4470bf0ef
Nick Muerdter <nick.muerdter@nrel.gov>
2016-10-03 20:12:46 -0600

    Cleanup and simplify assets for login page.
    Since the web-app's only assets are down to the simple login page, this
    removes the need for jQuery and Bootstrap on these pages.
    
    This also cleans up the styling of the login page a bit after the
    bootstrap 3 upgrade.
    
    Also fixes a long-standing bug where Persona would redirect to
    non-existent "#{failure_path}" after login failures (rather than a real
    url). Just in time for us to eventually remove Persona...

--- src/api-umbrella/web-app/app/assets/javascripts/admin/server_side_loader.js
+++ src/api-umbrella/web-app/app/assets/javascripts/admin/server_side_loader.js
@@ -1,3 +1,3 @@
 //= require i18n.js
 //= require i18n/translations.js
-//= require admin/common_validations.js
+//= require admin/_common_validations.js
--- src/api-umbrella/web-app/config/initializers/assets.rb
+++ src/api-umbrella/web-app/config/initializers/assets.rb
@@ -8,7 +8,9 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += [
+  "admin/login.css",
+]
 
 # Move default assets directory so this project can co-exist with the
 # static-site projectt that delivers most of the web content.
