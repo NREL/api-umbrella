fca4522260c65bb48833c2cc501e5f068c9dd222
Nick Muerdter <nick.muerdter@nrel.gov>
2016-11-13 15:23:15 -0700

    Transfer over more tests to new test suite.
    These contained some duplicative tests for API role checks that are no
    longer needed now that all of our API tests go through the full stack
    (so that functionality was already being tested).

---
+++ test/apis/v1/config/test_live_changes.rb
@@ -0,0 +1,108 @@
+require_relative "../../../test_helper"
+
+class TestApisV1ConfigLiveChanges < Minitest::Capybara::Test
+  include ApiUmbrellaTests::AdminAuth
+  include ApiUmbrellaTests::Setup
+  parallelize_me!
+
+  def setup
+    setup_server
+  end
+
+  def test_detects_published_api_changes_within_1_second
+    # Ensure that we hit the default routing.
+    response = Typhoeus.get("https://127.0.0.1:9081/#{unique_test_id}/info/", self.http_options)
+    assert_equal(404, response.code, response.body)
+
+    # Create a new API backend (but don't publish yet).
+    api_attributes = FactoryGirl.attributes_for(:api, {
+      :frontend_host => "127.0.0.1",
+      :backend_host => "127.0.0.1",
+      :servers => [{ :host => "127.0.0.1", :port => 9444 }],
+      :url_matches => [{ :frontend_prefix => "/#{unique_test_id}/", :backend_prefix => "/" }],
+      :settings => {
+        :headers => [
+          { :key => "X-New-Api", :value => "test1" },
+        ],
+      },
+    })
+    response = Typhoeus.post("https://127.0.0.1:9081/api-umbrella/v1/apis.json", @@http_options.deep_merge(admin_token).deep_merge({
+      :headers => { "Content-Type" => "application/json" },
+      :body => MultiJson.dump(:api => api_attributes),
+    }))
+    assert_equal(201, response.code, response.body)
+    new_api = MultiJson.load(response.body)
+    assert(new_api["api"]["id"])
+
+    # Wait 1 second to ensure time for any backend changes to get picked up.
+    sleep 1.1
+
+    # Ensure that we still hit the default routing, since we haven't published
+    # the new API backend.
+    response = Typhoeus.get("https://127.0.0.1:9081/#{unique_test_id}/info/", self.http_options)
+    assert_equal(404, response.code, response.body)
+
+    # Publish the API backend changes.
+    response = Typhoeus.post("https://127.0.0.1:9081/api-umbrella/v1/config/publish.json", @@http_options.deep_merge(admin_token).deep_merge({
+      :headers => { "Content-Type" => "application/json" },
+      :body => MultiJson.dump({
+        :config => {
+          :apis => {
+            new_api["api"]["id"] => { :publish => 1 },
+          },
+        },
+      }),
+    }))
+    assert_equal(201, response.code, response.body)
+
+    # Wait 1 second to ensure time for any backend changes to get picked up.
+    sleep 1.1
+
+    # The request to the new endpoint should now succeed.
+    response = Typhoeus.get("https://127.0.0.1:9081/#{unique_test_id}/info/", self.http_options)
+    assert_equal(200, response.code, response.body)
+    data = MultiJson.load(response.body)
+    assert_equal("test1", data["headers"]["x-new-api"])
+
+    # Update the existing API backend.
+    api_attributes[:settings][:headers] = [{ :key => "X-Updated-Api", :value => "test2" }]
+    response = Typhoeus.put("https://127.0.0.1:9081/api-umbrella/v1/apis/#{new_api["api"]["id"]}.json", @@http_options.deep_merge(admin_token).deep_merge({
+      :headers => { "Content-Type" => "application/json" },
+      :body => MultiJson.dump(:api => api_attributes),
+    }))
+    assert_equal(204, response.code, response.body)
+
+    # Wait 1 second to ensure time for any backend changes to get picked up.
+    sleep 1.1
+
+    # Ensure the updates are not live, since we haven't published them yet.
+    response = Typhoeus.get("https://127.0.0.1:9081/#{unique_test_id}/info/", self.http_options)
+    assert_equal(200, response.code, response.body)
+    data = MultiJson.load(response.body)
+    assert_equal("test1", data["headers"]["x-new-api"])
+    refute(data["headers"]["x-updated-api"])
+
+    # Publish the API backend changes.
+    response = Typhoeus.post("https://127.0.0.1:9081/api-umbrella/v1/config/publish.json", @@http_options.deep_merge(admin_token).deep_merge({
+      :headers => { "Content-Type" => "application/json" },
+      :body => MultiJson.dump({
+        :config => {
+          :apis => {
+            new_api["api"]["id"] => { :publish => 1 },
+          },
+        },
+      }),
+    }))
+    assert_equal(201, response.code, response.body)
+
+    # Wait 1 second to ensure time for any backend changes to get picked up.
+    sleep 1.1
+
+    # Verify that the updated api backend changes are now live.
+    response = Typhoeus.get("https://127.0.0.1:9081/#{unique_test_id}/info/", self.http_options)
+    assert_equal(200, response.code, response.body)
+    data = MultiJson.load(response.body)
+    assert_equal("test2", data["headers"]["x-updated-api"])
+    refute(data["headers"]["x-new-api"])
+  end
+end
---
+++ test/apis/v1/users/test_live_changes.rb
@@ -0,0 +1,125 @@
+require_relative "../../../test_helper"
+
+class TestApisV1UsersLiveChanges < Minitest::Capybara::Test
+  include ApiUmbrellaTests::AdminAuth
+  include ApiUmbrellaTests::Setup
+  parallelize_me!
+
+  def setup
+    setup_server
+  end
+
+  def test_created_api_keys_can_be_used_immediately
+    response = Typhoeus.post("https://127.0.0.1:9081/api-umbrella/v1/users.json", self.http_options.deep_merge(admin_token).deep_merge({
+      :headers => { "Content-Type" => "application/x-www-form-urlencoded" },
+      :body => { :user => FactoryGirl.attributes_for(:api_user) },
+    }))
+    assert_equal(201, response.code, response.body)
+    new_user = MultiJson.load(response.body)
+
+    response = Typhoeus.get("https://127.0.0.1:9081/api/info/", self.http_options.deep_merge({
+      :headers => { "X-Api-Key" => new_user["user"]["api_key"] },
+    }))
+    assert_equal(200, response.code, response.body)
+    data = MultiJson.load(response.body)
+    assert(new_user["user"]["id"])
+    assert_equal(new_user["user"]["id"], data["headers"]["x-api-user-id"])
+  end
+
+  def test_detects_role_changes_within_2_seconds
+    prepend_api_backends([
+      {
+        :frontend_host => "127.0.0.1",
+        :backend_host => "127.0.0.1",
+        :servers => [{ :host => "127.0.0.1", :port => 9444 }],
+        :url_matches => [{ :frontend_prefix => "/#{unique_test_id}/restricted-info/", :backend_prefix => "/info/" }],
+        :settings => { :required_roles => ["restricted"] },
+      },
+    ]) do
+      user = FactoryGirl.create(:api_user)
+
+      # Wait 2 seconds so we know the initial key created for this test has
+      # already been seen by the background task that clears the cache.
+      sleep 2.1
+
+      # Ensure that the key works as expected for an initial request.
+      response = Typhoeus.get("https://127.0.0.1:9081/api/info/", self.http_options.deep_merge({
+        :headers => { "X-Api-Key" => user.api_key },
+      }))
+      assert_equal(200, response.code, response.body)
+      data = MultiJson.load(response.body)
+      assert_equal(user.id, data["headers"]["x-api-user-id"])
+      refute(data["headers"]["x-api-roles"])
+
+      # Ensure that the key is rejected from a restricted endpoint.
+      response = Typhoeus.get("https://127.0.0.1:9081/#{unique_test_id}/restricted-info/", self.http_options.deep_merge({
+        :headers => { "X-Api-Key" => user.api_key },
+      }))
+      assert_equal(403, response.code, response.body)
+
+      # Update the key using the API to add the restricted role.
+      response = Typhoeus.put("https://127.0.0.1:9081/api-umbrella/v1/users/#{user.id}.json", self.http_options.deep_merge(admin_token).deep_merge({
+        :headers => { "Content-Type" => "application/json" },
+          :body => MultiJson.dump(:user => { :roles => ["restricted"] }),
+      }))
+      assert_equal(200, response.code, response.body)
+
+      # Wait 2 seconds to ensure the existing cache for this key get purged.
+      sleep 2.1
+
+      # The request to the restricted endpoint should now succeed. If it
+      # doesn't, the cache purging may not be working as expected.
+      response = Typhoeus.get("https://127.0.0.1:9081/#{unique_test_id}/restricted-info/", self.http_options.deep_merge({
+        :headers => { "X-Api-Key" => user.api_key },
+      }))
+      assert_equal(200, response.code, response.body)
+      data = MultiJson.load(response.body)
+      assert_equal(user.id, data["headers"]["x-api-user-id"])
+      assert_equal("restricted", data["headers"]["x-api-roles"])
+    end
+  end
+
+  def test_detects_rate_limit_changes_within_2_seconds
+    user = FactoryGirl.create(:api_user)
+
+    # Wait 2 seconds so we know the initial key created for this test has
+    # already been seen by the background task that clears the cache.
+    sleep 2.1
+
+    # Ensure that the key works as expected for an initial request.
+    response = Typhoeus.get("https://127.0.0.1:9081/api/info/", self.http_options.deep_merge({
+      :headers => { "X-Api-Key" => user.api_key },
+    }))
+    assert_equal(200, response.code, response.body)
+    data = MultiJson.load(response.body)
+    assert_equal(user.id, data["headers"]["x-api-user-id"])
+    assert_equal("1000", response.headers["x-ratelimit-limit"])
+
+    # Update the key using the API to add the restricted role.
+    response = Typhoeus.put("https://127.0.0.1:9081/api-umbrella/v1/users/#{user.id}.json", self.http_options.deep_merge(admin_token).deep_merge({
+      :headers => { "Content-Type" => "application/json" },
+      :body => MultiJson.dump(:user => {
+        :settings => {
+          :rate_limit_mode => "custom",
+          :rate_limits => [
+            FactoryGirl.attributes_for(:api_rate_limit, :limit => 10, :response_headers => true),
+          ],
+        },
+      }),
+    }))
+    assert_equal(200, response.code, response.body)
+
+    # Wait 2 seconds to ensure the existing cache for this key get purged.
+    sleep 2.1
+
+    # The request to the restricted endpoint should now succeed. If it
+    # doesn't, the cache purging may not be working as expected.
+    response = Typhoeus.get("https://127.0.0.1:9081/api/info/", self.http_options.deep_merge({
+      :headers => { "X-Api-Key" => user.api_key },
+    }))
+    assert_equal(200, response.code, response.body)
+    data = MultiJson.load(response.body)
+    assert_equal(user.id, data["headers"]["x-api-user-id"])
+    assert_equal("10", response.headers["x-ratelimit-limit"])
+  end
+end
