b540ef9e0c049b2faf79a3a4975db58962db6b36
Nick Muerdter <nick.muerdter@nrel.gov>
2016-11-27 13:28:07 -0700

    Fix incorrect shellcheck binary name.

--- build/cmake/test/shellcheck.cmake
+++ build/cmake/test/shellcheck.cmake
@@ -2,7 +2,7 @@ ExternalProject_Add(
   shellcheck
   URL https://github.com/caarlos0/shellcheck-docker/releases/download/v${SHELLCHECK_VERSION}/shellcheck
   URL_HASH MD5=${SHELLCHECK_HASH}
-  DOWNLOAD_NAME shellcheck-binz
+  DOWNLOAD_NAME shellcheck
   DOWNLOAD_NO_EXTRACT 1
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
