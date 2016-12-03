754d95fac3d6601e31988c43ec7220969323c424
Nick Muerdter <nick.muerdter@nrel.gov>
2016-12-03 14:14:18 -0700

    Symlink the node and bower dependencies into place within the source.

--- build/cmake/core-admin-ui.cmake
+++ build/cmake/core-admin-ui.cmake
@@ -55,3 +55,31 @@ add_custom_command(
   COMMAND cd ${CORE_BUILD_DIR}/tmp/admin-ui-build && env PATH=${DEV_INSTALL_PREFIX}/bin:$ENV{PATH} ./node_modules/.bin/ember build --environment=production --output-path=./dist-prod
   COMMAND touch ${STAMP_DIR}/core-admin-ui-build
 )
+
+# Normally we perform the yarn & bower installs out-of-source (so the build
+# takes place entirely out of source), but if testing/development is enabled
+# for this build, then also create a local symlink within the source. This then
+# allows for easier interactions with the application.
+if(ENABLE_TEST_DEPENDENCIES)
+  add_custom_command(
+    OUTPUT ${CMAKE_SOURCE_DIR}/src/api-umbrella/admin-ui/node_modules
+    DEPENDS
+      ${STAMP_DIR}/core-admin-ui-yarn-install
+      ${CORE_BUILD_DIR}/tmp/admin-ui-build/node_modules
+    COMMAND rm -rf ${CMAKE_SOURCE_DIR}/src/api-umbrella/admin-ui/node_modules
+    COMMAND ln -snf ${CORE_BUILD_DIR}/tmp/admin-ui-build/node_modules ${CMAKE_SOURCE_DIR}/src/api-umbrella/admin-ui/node_modules
+    COMMAND touch -h ${CMAKE_SOURCE_DIR}/src/api-umbrella/admin-ui/node_modules
+  )
+  add_custom_target(core-admin-ui-local-yarn ALL DEPENDS ${CMAKE_SOURCE_DIR}/src/api-umbrella/admin-ui/node_modules)
+
+  add_custom_command(
+    OUTPUT ${CMAKE_SOURCE_DIR}/src/api-umbrella/admin-ui/bower_components
+    DEPENDS
+      ${STAMP_DIR}/core-admin-ui-bower-install
+      ${CORE_BUILD_DIR}/tmp/admin-ui-build/bower_components
+    COMMAND rm -rf ${CMAKE_SOURCE_DIR}/src/api-umbrella/admin-ui/bower_components
+    COMMAND ln -snf ${CORE_BUILD_DIR}/tmp/admin-ui-build/bower_components ${CMAKE_SOURCE_DIR}/src/api-umbrella/admin-ui/bower_components
+    COMMAND touch -h ${CMAKE_SOURCE_DIR}/src/api-umbrella/admin-ui/bower_components
+  )
+  add_custom_target(core-admin-ui-local-bower ALL DEPENDS ${CMAKE_SOURCE_DIR}/src/api-umbrella/admin-ui/bower_components)
+endif()
