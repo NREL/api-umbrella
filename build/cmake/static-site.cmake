ee1bd37a1c26afa66ff8fddaeb8098141013df02
Nick Muerdter <nick.muerdter@nrel.gov>
2016-12-12 09:21:10 -0700

    Fix static site build in non-development environments.
    Node needed to be on the PATH for asset precompiling in execjs to work.

--- build/cmake/static-site.cmake
+++ build/cmake/static-site.cmake
@@ -6,7 +6,7 @@ ExternalProject_Add(
   URL_HASH MD5=${API_UMBRELLA_STATIC_SITE_HASH}
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND env PATH=${STAGE_EMBEDDED_DIR}/bin:$ENV{PATH} bundle install --path=<SOURCE_DIR>/vendor/bundle
-  BUILD_COMMAND env PATH=${STAGE_EMBEDDED_DIR}/bin:$ENV{PATH} bundle exec middleman build
+  BUILD_COMMAND env PATH=${DEV_INSTALL_PREFIX}/bin:${STAGE_EMBEDDED_DIR}/bin:$ENV{PATH} bundle exec middleman build
   INSTALL_COMMAND rm -rf ${STAGE_EMBEDDED_DIR}/apps/static-site/releases
     COMMAND mkdir -p ${STAGE_EMBEDDED_DIR}/apps/static-site/releases/0/build
     COMMAND rsync -a <SOURCE_DIR>/build/ ${STAGE_EMBEDDED_DIR}/apps/static-site/releases/0/build/
