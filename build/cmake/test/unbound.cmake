262cbea40ee0491218f3e87b8e109c0d425661a8
Nick Muerdter <nick.muerdter@nrel.gov>
2017-02-05 08:58:39 -0700

    Try to fix building unbound in clean environment.
    I think something must have changed in newer versions of unbound, since
    building now fails with an out of source build due to configlexer path
    issues.

--- build/cmake/test/unbound.cmake
+++ build/cmake/test/unbound.cmake
@@ -3,6 +3,6 @@ ExternalProject_Add(
   unbound
   URL http://www.unbound.net/downloads/unbound-${UNBOUND_VERSION}.tar.gz
   URL_HASH SHA256=${UNBOUND_HASH}
+  BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${TEST_INSTALL_PREFIX}
-  INSTALL_COMMAND make install
 )
