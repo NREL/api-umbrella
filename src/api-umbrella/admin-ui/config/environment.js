c3afbad7a5a1612f3e9a81444d17d8f7c9d6ae2d
Nick Muerdter <nick.muerdter@nrel.gov>
2017-02-19 11:42:00 -0700

    Fix baseURL deprecation and update livereload customizations.
    - Fix the baseURL deprecation in current version of Ember. Replace with
      rootURL.
    - Our livereload customizations were using baseURL, so update how we
      customize livereload's config. Newer versions of the plugin allow for
      easier customization, so bring back the default version of the plugin,
      and tweak how we implement our HTTPS proxy config to work with the
      updates.

--- src/api-umbrella/admin-ui/config/environment.js
+++ src/api-umbrella/admin-ui/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'api-umbrella-admin-ui',
     environment: environment,
-    baseURL: '/admin/',
+    rootURL: '/admin/',
     locationType: 'hash',
     EmberENV: {
       FEATURES: {
@@ -29,7 +29,6 @@ module.exports = function(environment) {
 
   if(environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
