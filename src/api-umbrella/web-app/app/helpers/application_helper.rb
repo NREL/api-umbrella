6438a71eef0c95223818e76d1dbeae7e09abc9c4
Nick Muerdter <nick.muerdter@nrel.gov>
2017-02-19 07:22:30 -0700

    Remove defunct database seeding from web-app.
    This has been moved into the Lua code
    (src/api-umbrella/proxy/startup/seed_database.lua) for a while now. Just
    cleaning up this old, unused seeding code.

--- src/api-umbrella/web-app/app/helpers/application_helper.rb
+++ src/api-umbrella/web-app/app/helpers/application_helper.rb
@@ -15,20 +15,7 @@ module ApplicationHelper
   end
 
   def web_admin_ajax_api_user
-    user = ApiUser.where(:email => "web.admin.ajax@internal.apiumbrella").order_by(:created_at.asc).first
-    unless(user)
-      user = ApiUser.create!({
-        :email => "web.admin.ajax@internal.apiumbrella",
-        :first_name => "API Umbrella Admin",
-        :last_name => "Key",
-        :use_description => "An API key for the API Umbrella admin to use for internal ajax requests.",
-        :terms_and_conditions => "1",
-        :registration_source => "seed",
-        :settings_attributes => { :rate_limit_mode => "unlimited" },
-      }, :without_protection => true)
-    end
-
-    user
+    ApiUser.where(:email => "web.admin.ajax@internal.apiumbrella").order_by(:created_at.asc).first
   end
 
   def omniauth_external_providers
