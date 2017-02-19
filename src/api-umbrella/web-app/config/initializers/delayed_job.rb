837ca8f1f344528add7060101d1e8c4cd575d2a9
Nick Muerdter <nick.muerdter@nrel.gov>
2017-02-19 15:55:27 -0700

    Fix api-umbrella process stop sometimes taking a long time.

--- src/api-umbrella/web-app/config/initializers/delayed_job.rb
+++ src/api-umbrella/web-app/config/initializers/delayed_job.rb
@@ -1,3 +1,13 @@
+# Exit immediately when the delayed_job process receives a kill signal.
+#
+# This prevents some race conditions that can lead to API Umbrella stopping
+# taking a long time. If mongo is running on the same server and mongo exits
+# first, then without exiting immediately, delayed_job can get stuck waiting
+# for a new mongo primary (until the 30s server_selection_timeout is hit)
+# before exiting.
+Delayed::Worker.raise_signal_exceptions = true
+
+# Keep failed jobs in the database.
 Delayed::Worker.destroy_failed_jobs = false
 
 if(Rails.env.test?)
