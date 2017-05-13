017d3d3a2f98bca624c60b1782b154e0fc788049
Nick Muerdter <nick.muerdter@nrel.gov>
2017-05-13 14:00:13 -0600

    Use awesome-print for even prettier test diffs.

--- test/support/pretty_diffs.rb
+++ test/support/pretty_diffs.rb
@@ -1,2 +1,13 @@
-# More readable diffs for hashes/arrays/objects when tests fail.
-Minitest::Test.make_my_diffs_pretty!
+class Minitest::Test
+  # Override the method minitest uses for generating diffs to use
+  # awesome_inspect. This provides easier to read diffs, particularly when
+  # dealing with nested, complex hashes.
+  def mu_pp(obj)
+    obj.awesome_inspect({
+      :indent => -2,
+      :index => false,
+      :sort_keys => true,
+      :sort_vars => true,
+    })
+  end
+end
