84c62729a3e310a439b88fe8096792a74c339651
Nick Muerdter <nick.muerdter@nrel.gov>
2017-07-06 15:58:45 -0600

    Update dependencies.

--- build/cmake/test/shellcheck.cmake
+++ build/cmake/test/shellcheck.cmake
@@ -1,15 +1,8 @@
 ExternalProject_Add(
   shellcheck
-  # Download 64bit linux binary (that works in all distros) for v0.4.5 from
-  # author:
-  # https://github.com/koalaman/shellcheck/issues/758#issuecomment-257730652
-  #
-  # Should followup with shellcheck issue about whether they plan to release
-  # these binaries more formally and on an ongoing basis.
-  URL https://www.vidarholen.net/%7Evidar/shellcheck.${SHELLCHECK_VERSION}.gz
-  URL_HASH SHA256=${SHELLCHECK_HASH}
-  DOWNLOAD_NO_EXTRACT 1
+  URL https://storage.googleapis.com/shellcheck/shellcheck-v${SHELLCHECK_VERSION}.linux.x86_64.tar.xz
+  URL_HASH SHA512=${SHELLCHECK_HASH}
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND gzip -d -c <DOWNLOADED_FILE> > <BINARY_DIR>/shellcheck
-  INSTALL_COMMAND install -D -m 755 <BINARY_DIR>/shellcheck ${TEST_INSTALL_PREFIX}/bin/shellcheck
+  BUILD_COMMAND ""
+  INSTALL_COMMAND install -D -m 755 <SOURCE_DIR>/shellcheck ${TEST_INSTALL_PREFIX}/bin/shellcheck
 )
