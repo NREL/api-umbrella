0e86c300e1e9796df1e962382e55393e147c5397
Andres Muñoz <andres.max36@gmail.com>
2017-11-07 16:26:17 +0100

    Revert "New feature for OAuth2 Token Validation using Fiware Wilma PEP-Proxy"

--- src/api-umbrella/utils/invert_table.lua
+++ src/api-umbrella/utils/invert_table.lua
@@ -1,18 +1,8 @@
 return function(table)
-  local numItems = 0
   local inverted = {}
   for key, value in pairs(table) do
-    if type(value)=="string" then
-      inverted[value] = key
-    else
-      for k,v in pairs(value) do
-        numItems = numItems + 1
-      end
-      if numItems > 1 then
-        value = value["name"]
-      end
-      inverted[value] = key
-    end
+    inverted[value] = key
   end
+
   return inverted
 end
