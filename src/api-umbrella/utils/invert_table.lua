16604c72449e8dcfc4ebcfc45f0f023ccd513bca
Andres Muñoz <andres.max36@gmail.com>
2017-11-13 15:26:09 +0100

    This function usually is used for inverting the roles table, it was modified to allow and parse a roles tables that have more than one value by rol, for example: if the function receives the roles in this format Roles":[{"name":"rol1","id":"16"}].
    With the modification the function checks if the roles value has more than one value and takes only the value for the key "name" and after that, the function inverts the table.

--- src/api-umbrella/utils/invert_table.lua
+++ src/api-umbrella/utils/invert_table.lua
@@ -1,8 +1,18 @@
 return function(table)
+  local numItems = 0
   local inverted = {}
   for key, value in pairs(table) do
-    inverted[value] = key
+    if type(value)=="string" then
+      inverted[value] = key
+    else
+      for k,v in pairs(value) do
+        numItems = numItems + 1
+      end
+      if numItems > 1 then
+        value = value["name"]
+      end
+      inverted[value] = key
+    end
   end
-
   return inverted
 end
