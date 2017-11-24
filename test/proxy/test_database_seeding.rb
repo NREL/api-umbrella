bd8e5fd3ed1edc89c9f26e07b61570ddee167bf7
Nick Muerdter <nick.muerdter@nrel.gov>
2017-11-23 23:21:18 -0700

    More test suite fixes.

--- test/proxy/test_database_seeding.rb
+++ test/proxy/test_database_seeding.rb
@@ -13,69 +13,54 @@ class Test::Proxy::TestDatabaseSeeding < Minitest::Test
     users = ApiUser.where(:email => "static.site.ajax@internal.apiumbrella").all
     assert_equal(1, users.length)
 
-    user = users.first.attributes
-    assert_equal({
-      "email" => "static.site.ajax@internal.apiumbrella",
-      "first_name" => "API Umbrella Static Site",
-      "last_name" => "Key",
-      "registration_source" => "seed",
-      "roles" => ["api-umbrella-key-creator", "api-umbrella-contact-form"],
-      "terms_and_conditions" => "1",
-      "use_description" => "An API key for the API Umbrella static website to use for ajax requests.",
-    }, user.compact.except("_id", "created_at", "updated_at", "api_key", "settings"))
-    assert_match(/\A[0-9a-f\-]{36}\z/, user["_id"])
-    assert_kind_of(Time, user["created_at"])
-    assert_kind_of(Time, user["updated_at"])
+    response = Typhoeus.get("https://127.0.0.1:9081/api-umbrella/v1/users/#{users.first.id}.json", http_options.deep_merge(admin_token))
+    assert_response_code(200, response)
+    user = MultiJson.load(response.body).fetch("user")
+
+    assert_equal("static.site.ajax@internal.apiumbrella", user["email"])
+    assert_equal("API Umbrella Static Site", user["first_name"])
+    assert_equal("Key", user["last_name"])
+    assert_equal("seed", user["registration_source"])
+    assert_equal(["api-umbrella-key-creator", "api-umbrella-contact-form"].sort, user["roles"].sort)
+    assert_equal("An API key for the API Umbrella static website to use for ajax requests.", user["use_description"])
+    assert_match(/\A[0-9a-f\-]{36}\z/, user["id"])
+    assert_match_iso8601(user["created_at"])
+    assert_match_iso8601(user["updated_at"])
     assert_match(/\A[a-zA-Z0-9]{40}\z/, user["api_key"])
-
-    assert_equal({
-      "rate_limit_mode" => "custom",
-    }, user["settings"].compact.except("_id", "rate_limits"))
-    assert_match(/\A[0-9a-f\-]{36}\z/, user["settings"]["_id"])
-
-    assert_equal([
-      {
-        "accuracy" => 5000.0,
-        "duration" => 60000.0,
-        "limit" => 5.0,
-        "limit_by" => "ip",
-        "response_headers" => false,
-      },
-      {
-        "accuracy" => 60000.0,
-        "duration" => 3600000.0,
-        "limit" => 20.0,
-        "limit_by" => "ip",
-        "response_headers" => true,
-      },
-    ], user["settings"]["rate_limits"].map { |r| r.compact.except("_id") })
-    assert_match(/\A[0-9a-f\-]{36}\z/, user["settings"]["rate_limits"][0]["_id"])
-    assert_match(/\A[0-9a-f\-]{36}\z/, user["settings"]["rate_limits"][1]["_id"])
+    assert_match(/\A[0-9a-f\-]{36}\z/, user["settings"]["id"])
+    assert_equal("custom", user["settings"]["rate_limit_mode"])
+    assert_equal(5000, user["settings"]["rate_limits"][0]["accuracy"])
+    assert_equal(60000, user["settings"]["rate_limits"][0]["duration"])
+    assert_equal(5, user["settings"]["rate_limits"][0]["limit"])
+    assert_equal("ip", user["settings"]["rate_limits"][0]["limit_by"])
+    assert_equal(false, user["settings"]["rate_limits"][0]["response_headers"])
+    assert_equal(60000, user["settings"]["rate_limits"][1]["accuracy"])
+    assert_equal(3600000, user["settings"]["rate_limits"][1]["duration"])
+    assert_equal(20, user["settings"]["rate_limits"][1]["limit"])
+    assert_equal("ip", user["settings"]["rate_limits"][1]["limit_by"])
+    assert_equal(true, user["settings"]["rate_limits"][1]["response_headers"])
   end
 
   def test_api_key_for_admin
     users = ApiUser.where(:email => "web.admin.ajax@internal.apiumbrella").all
     assert_equal(1, users.length)
 
-    user = users.first.attributes
-    assert_equal({
-      "email" => "web.admin.ajax@internal.apiumbrella",
-      "first_name" => "API Umbrella Admin",
-      "last_name" => "Key",
-      "registration_source" => "seed",
-      "roles" => ["api-umbrella-key-creator"],
-      "terms_and_conditions" => "1",
-      "use_description" => "An API key for the API Umbrella admin to use for internal ajax requests.",
-    }, user.compact.except("_id", "created_at", "updated_at", "api_key", "settings"))
-    assert_match(/\A[0-9a-f\-]{36}\z/, user["_id"])
-    assert_kind_of(Time, user["created_at"])
-    assert_kind_of(Time, user["updated_at"])
-    assert_match(/\A[a-zA-Z0-9]{40}\z/, user["api_key"])
+    response = Typhoeus.get("https://127.0.0.1:9081/api-umbrella/v1/users/#{users.first.id}.json", http_options.deep_merge(admin_token))
+    assert_response_code(200, response)
+    user = MultiJson.load(response.body).fetch("user")
 
-    assert_equal({
-      "rate_limit_mode" => "unlimited",
-    }, user["settings"].compact.except("_id"))
-    assert_match(/\A[0-9a-f\-]{36}\z/, user["settings"]["_id"])
+    assert_equal("web.admin.ajax@internal.apiumbrella", user["email"])
+    assert_equal("API Umbrella Admin", user["first_name"])
+    assert_equal("Key", user["last_name"])
+    assert_equal("seed", user["registration_source"])
+    assert_equal(["api-umbrella-key-creator"], user["roles"])
+    assert_equal("An API key for the API Umbrella admin to use for internal ajax requests.", user["use_description"])
+    assert_match(/\A[0-9a-f\-]{36}\z/, user["id"])
+    assert_match_iso8601(user["created_at"])
+    assert_match_iso8601(user["updated_at"])
+    assert_match(/\A[a-zA-Z0-9]{40}\z/, user["api_key"])
+    assert_match(/\A[0-9a-f\-]{36}\z/, user["settings"]["id"])
+    assert_equal("unlimited", user["settings"]["rate_limit_mode"])
   end
 
   def test_admin_permission_records
@@ -101,7 +86,7 @@ class Test::Proxy::TestDatabaseSeeding < Minitest::Test
     ].sort, permissions.map { |p| p.name }.sort)
 
     permission = permissions[0].attributes
-    assert_kind_of(String, permission["_id"])
+    assert_kind_of(String, permission["id"])
     assert_kind_of(String, permission["name"])
     assert_kind_of(Numeric, permission["display_order"])
     assert_kind_of(Time, permission["created_at"])
