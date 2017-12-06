c906c90b1cfc9b0b269ef56e3ddbd2a84faa1181
Nick Muerdter <nick.muerdter@nrel.gov>
2017-12-06 07:36:36 -0700

    Fix connecting to password protected elasticsearch servers.

--- src/api-umbrella/utils/elasticsearch.lua
+++ src/api-umbrella/utils/elasticsearch.lua
@@ -2,32 +2,56 @@ local http = require "resty.http"
 local json_decode = require("cjson").decode
 local json_encode = require "api-umbrella.utils.json_encode"
 
-local elasticsearch_host = config["elasticsearch"]["hosts"][1]
+local server = config["elasticsearch"]["_first_server"]
 
 local _M = {}
 
 function _M.query(path, options)
   local httpc = http.new()
 
+  if not options then
+    options = {}
+  end
+
+  options["path"] = path
+
+  if not options["headers"] then
+    options["headers"] = {}
+  end
+
+  if server["userinfo"] and not options["headers"]["Authorization"] then
+     options["headers"]["Authorization"] = "Basic " .. ngx.encode_base64(server["userinfo"])
+  end
+
   if options and options["body"] and type(options["body"]) == "table" then
     options["body"] = json_encode(options["body"])
 
-    if not options["headers"] then
-      options["headers"] = {}
-    end
     if not options["headers"]["Content-Type"] then
       options["headers"]["Content-Type"] = "application/json"
     end
   end
 
-  local res, err = httpc:request_uri(elasticsearch_host .. path, options)
+  httpc:connect(server["host"], server["port"])
+  local res, err = httpc:request(options)
   if err then
     return nil, err
-  elseif res.status >= 500 then
-    return nil, "Unsuccessful response: " .. (res.body or "")
+  end
+
+  local body, body_err = res:read_body()
+  if body_err then
+    return nil, body_err
+  end
+
+  local keepalive_ok, keepalive_err = httpc:set_keepalive()
+  if not keepalive_ok then
+    ngx.log(ngx.ERR, keepalive_err)
+  end
+
+  if res.status >= 500 then
+    return nil, "Unsuccessful response: " .. (body or "")
   else
     if res.headers["Content-Type"] and string.sub(res.headers["Content-Type"], 1, 16) == "application/json" then
-      res["body_json"] = json_decode(res.body)
+      res["body_json"] = json_decode(body)
     end
 
     return res
