c7caa6504b69f6fc89285d452cb76b338d8e8f61
Nick Muerdter <nick.muerdter@nrel.gov>
2018-05-28 08:17:00 -0600

    Use override_public_https_proto setting in httpsify_current_url helper.

--- src/api-umbrella/utils/httpsify_current_url.lua
+++ src/api-umbrella/utils/httpsify_current_url.lua
@@ -1,13 +1,19 @@
 return function()
-  local https_url = "https://" .. ngx.ctx.host_normalized
+  local https_url = {
+    config["override_public_https_proto"] or "https",
+    "://",
+    ngx.ctx.host_normalized,
+  }
   if config["override_public_https_port"] then
     if config["override_public_https_port"] ~= 443 then
-      https_url = https_url .. ":" .. config["override_public_https_port"]
+      table.insert(https_url, ":")
+      table.insert(https_url, config["override_public_https_port"])
     end
   elseif config["https_port"] and config["https_port"] ~= 443 then
-    https_url = https_url .. ":" .. config["https_port"]
+    table.insert(https_url, ":")
+    table.insert(https_url, config["https_port"])
   end
-  https_url = https_url .. ngx.ctx.original_request_uri
+  table.insert(https_url, ngx.ctx.original_request_uri)
 
-  return https_url
+  return table.concat(https_url, "")
 end
