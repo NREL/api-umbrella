9109f7c9c748a37289cba89cb8981439de552684
Nick Muerdter <nick.muerdter@nrel.gov>
2018-06-10 21:27:00 -0600

    More experiments with better build caching.

--- Makefile
+++ Makefile
@@ -1,5 +1,19 @@
-%: all
-	@true
+# A minimal Makefile that downloads the "task" tool and passes things off to
+# that tool for executing (so "make all" actually becomes "task all"). See
+# Taskfile.yml for more detail.
 
-all:
-	/app/task $(MAKECMDGOALS)
+ROOT_DIR:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+WORK_DIR:=$(ROOT_DIR)/build/work
+
+.PHONY: default
+default: all
+
+# Download and locally install the "task" tool.
+TASK_VERSION:=2.0.2
+$(ROOT_DIR)/tasks/bootstrap-$(TASK_VERSION): ;
+$(WORK_DIR)/stamp/bootstrap-$(TASK_VERSION): $(ROOT_DIR)/tasks/bootstrap-$(TASK_VERSION)
+	$(ROOT_DIR)/tasks/bootstrap-$(TASK_VERSION)
+
+# Match all commands sent to "make" and send them to "task" instead.
+%: $(WORK_DIR)/stamp/bootstrap-$(TASK_VERSION)
+	$(WORK_DIR)/task $(MAKECMDGOALS)
--- tasks/helpers.sh
+++ tasks/helpers.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set +x
+
 SOURCE_DIR="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")" && pwd)"
 
 INSTALL_PREFIX=/opt/api-umbrella
@@ -32,22 +33,74 @@ STAGE_EMBEDDED_PATH="$STAGE_EMBEDDED_DIR/bin:$DEFAULT_PATH"
 DEV_PATH="$DEV_INSTALL_PREFIX/bin:$STAGE_EMBEDDED_PATH"
 
 # Where to install app-level vendor dependencies.
-VENDOR_DIR="$WORK_DIR/vendor"
-VENDOR_LUA_DIR="$VENDOR_DIR/share/lua/5.1"
+APP_CORE_DIR="$STAGE_EMBEDDED_DIR/apps/core"
+APP_CORE_RELEASE_DIR="$APP_CORE_DIR/releases/0"
+APP_CORE_VENDOR_DIR="$APP_CORE_DIR/shared/vendor"
+APP_CORE_VENDOR_BUNDLE_DIR="$APP_CORE_VENDOR_DIR/bundle"
+APP_CORE_VENDOR_LUA_DIR="$APP_CORE_VENDOR_DIR/lua"
+APP_CORE_VENDOR_LUA_SHARE_DIR="$APP_CORE_VENDOR_LUA_DIR/share/lua/5.1"
 LUA_PREFIX="$STAGE_EMBEDDED_DIR"
-LUAROCKS_CMD=(env LUA_PATH="$LUA_PREFIX/openresty/luajit/share/lua/5.1/?.lua;$LUA_PREFIX/openresty/luajit/share/lua/5.1/?/init.lua;;" "$LUA_PREFIX/bin/luarocks")
-OPM_CMD=(env LUA_PATH="$LUA_PREFIX/openresty/lualib/?.lua;$LUA_PREFIX/openresty/lualib/?/init.lua;;" PATH="$STAGE_EMBEDDED_PATH" LD_LIBRARY_PATH="$STAGE_EMBEDDED_DIR/openresty/luajit/lib:$STAGE_EMBEDDED_DIR/lib" opm)
-set -x
+LUAROCKS_CMD=(env "LUA_PATH=$LUA_PREFIX/openresty/luajit/share/lua/5.1/?.lua;$LUA_PREFIX/openresty/luajit/share/lua/5.1/?/init.lua;;" "$LUA_PREFIX/bin/luarocks")
+OPM_CMD=(env "LUA_PATH=$LUA_PREFIX/openresty/lualib/?.lua;$LUA_PREFIX/openresty/lualib/?/init.lua;;" "PATH=$STAGE_EMBEDDED_PATH" "LD_LIBRARY_PATH=$STAGE_EMBEDDED_DIR/openresty/luajit/lib:$STAGE_EMBEDDED_DIR/lib" opm)
 
+# Determine the sub-path for the currently executing task. This can be used for
+# generating unique directories for the current task.
+#
+# For example, ./tasks/deps/openresty's subpath would be "deps/openresty"
+TASK_SUBPATH="${BASH_SOURCE[1]#*tasks/}"
+
+# Creating a working directory for the currently running task under
+# ./build/work/tasks/*
+#
+# For example, ./tasks/deps/openresty's working directory would be
+# ./build/work/tasks/deps/openresty
 task_working_dir() {
   set +x
-  task_path=${0#*tasks/}
-  dir="$WORK_DIR/tasks/$task_path"
+  dir="$WORK_DIR/tasks/$TASK_SUBPATH"
   set -x
+
+  # Make the directory and cd into it.
   mkdir -p "$dir"
   cd "$dir" || exit 1
 
-  find "$dir" -mindepth 1 -maxdepth 1 -not -name "downloads" -exec rm -rf {} \;
+  # Cleanup any files not in the special "_persist" directory before running
+  # the rest of the task. This ensures clean builds if a task is being executed
+  # (since we assume the task script is only being executed if the checksum has
+  # changed). The persist directory is used for some items, like downloads,
+  # just to speed-up development rebuilds a bit.
+  find "$dir" -mindepth 1 -maxdepth 1 -not -name "_persist" -print -exec rm -rf {} \;
+}
+
+# Generate a stamp file indicating a task has successfully run. This should be
+# the last line in each task file.
+#
+# In conjunction with go-task's checksum and dependency functionality, these
+# stamp files ensure that each task has an output file that can be setup as a
+# dependency for other tasks. Since each task will only run if necessary (due
+# to go-tasks's checksumming), these stamp files should change on every
+# successful run so that any dependencies of the current task also get
+# triggered (in a cascading fashion).
+stamp() {
+  set +x
+  stamp_path="$WORK_DIR/stamp/$TASK_SUBPATH"
+  stamp_dir=$(dirname "$stamp_path")
+
+  # Remove the previous stamp. Also try removing the parent stamp_dir if it's
+  # actually a file (to handle situations where the task is shifted into or out
+  # of a subdirectory).
+  rm -rf "$stamp_path"
+  if [ -f "$stamp_dir" ]; then
+    rm -f "$stamp_dir"
+  fi
+  mkdir -p "$stamp_dir"
+
+  # Generate random content within the stamp file to ensure it's checksum will
+  # change on each successful run.
+  echo "Stamp: $stamp_path"
+  date > "$stamp_path"
+  openssl rand -hex 64 >> "$stamp_path"
+
+  set -x
 }
 
 download() {
@@ -56,19 +109,19 @@ download() {
   hash_algorithm=$2
   expected_hash=$3
 
+  # Download the file.
   filename=$(basename "$url")
-  downloads_dir="$(pwd)/downloads"
+  downloads_dir="$(pwd)/_persist/downloads"
   download_path="$downloads_dir/$filename"
   mkdir -p "$downloads_dir"
-
   if [ ! -f "$download_path" ]; then
     set -x
     curl --location --retry 3 --fail --output "$download_path" "$url"
     set +x
   fi
 
+  # Verify the checksum of the downloaded file.
   actual_hash=$(openssl dgst -"$hash_algorithm" "$download_path" | awk '{print $2}')
-
   if [ "$expected_hash" != "$actual_hash" ]; then
     echo "Checksum for $download_path did not match"
     echo "  Expected hash: $expected_hash"
@@ -80,7 +133,7 @@ download() {
 }
 
 extract_download() {
-  tar -xf "downloads/$1"
+  tar -xf "_persist/downloads/$1"
 }
 
 _luarocks_install() {
@@ -88,33 +141,40 @@ _luarocks_install() {
   package="$2"
   version="$3"
 
+  set -x
   "${LUAROCKS_CMD[@]}" --tree="$tree_dir" install "$package" "$version"
-  find "$tree_dir/lib/lua" -name "*.so" -exec chrpath -d {} \;
+  find "$tree_dir/lib" -name "*.so" -exec chrpath -d {} \;
 }
 
 luarocks_install() {
-  _luarocks_install "$VENDOR_DIR" "$@"
+  set +x
+  _luarocks_install "$APP_CORE_VENDOR_LUA_DIR" "$@"
 }
 
 test_luarocks_install() {
+  set +x
   _luarocks_install "$TEST_VENDOR_DIR" "$@"
 }
 
 _opm_install() {
   tree_dir="$1"
   package="$2"
-  account="$3"
-  version="$4"
+  version="$3"
 
+  set -x
   mkdir -p "$tree_dir"
-  (cd "$tree_dir" && "${OPM_CMD[@]}" --cwd get "$account/$package=$version")
+  (cd "$tree_dir" && "${OPM_CMD[@]}" --cwd get "$package=$version")
   find "$tree_dir/resty_modules" -name "*.so" -exec chrpath -d {} \;
 }
 
 opm_install() {
-  _opm_install "$VENDOR_DIR" "$@"
+  set +x
+  _opm_install "$APP_CORE_VENDOR_LUA_DIR" "$@"
 }
 
 test_opm_install() {
+  set +x
   _opm_install "$TEST_VENDOR_DIR" "$@"
 }
+
+set -x
