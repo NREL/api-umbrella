5e7101f82ae7a1c3ca40a15d98c8ca25a19cb526
Nick Muerdter <nick.muerdter@nrel.gov>
2018-06-16 18:42:17 -0600

    Fix intermittent rsylog memory test failures.
    Hopefully this will finally settle this test for good, since rarely the
    memory growth is reported upwards of 12000kb. So change some things
    around, and check the overall memory of the process at the start to try
    and better test for this when running in the full suite.

--- test/processes/test_rsyslog.rb
+++ test/processes/test_rsyslog.rb
@@ -14,23 +14,37 @@ class Test::Processes::TestRsyslog < Minitest::Test
   def test_memory_leak
     # Make some initial requests, to ensure rsyslog is warmed up, which should
     # stabilize its memory use.
-    make_requests(2000)
+    make_requests(2_000)
     warmed_memory_use = memory_use
     warmed_error_log_size = elasticsearch_error_log_size
 
+    # Validate that the rsyslog process isn't using more than 100MB initially.
+    # If it is, then this is probably the best indicator that there's a memory
+    # leak and previous tests in the test suite have already leaked a lot of
+    # memory. Typically rsyslog is more in the 20MB-50MB range.
+    assert_operator(warmed_memory_use[:rss], :<, 100_000)
+
     # Make more requests.
-    make_requests(8000)
+    make_requests(10_000)
     final_memory_use = memory_use
     final_error_log_size = elasticsearch_error_log_size
 
-    # Compare the memory use before and after making requests. We're going to
-    # assume it should be not grow more than 4MB (we need to allow for some
-    # fluctuations under normal use).
+    # Compare the memory use before and after making requests. Note that this
+    # is a very loose test (allowing 15MB of growth), since there can be a
+    # decent amount of fluctuation in normal use. It's hard to determine
+    # whether there's really a leak in such a short test, so the better test is
+    # actually the initial check to ensure the process is less than 100MB after
+    # warmup. Assuming this test gets called as part of running the full test
+    # suite, and it's not one of the initial tests (which is randomized, but
+    # won't be at least enough times to catch this at some point), then that's
+    # a better test for the leak, since it allows us to detect leaks over
+    # longer periods of time (over the entire time the test suite is running,
+    # rather than just in this isolated test).
     rss_diff = final_memory_use.fetch(:rss) - warmed_memory_use.fetch(:rss)
-    assert_operator(rss_diff, :<=, 4096)
+    assert_operator(rss_diff, :<=, 15_000)
 
-    # Also ensure nothing was generated in the elasticsearch error log file,
-    # since the specific problem in v8.28.0 generated error data.
+    # Ensure nothing was generated in the elasticsearch error log file, since
+    # the specific leak in v8.28.0 was triggered by generated error data.
     assert_equal(warmed_error_log_size, final_error_log_size)
   end
 
