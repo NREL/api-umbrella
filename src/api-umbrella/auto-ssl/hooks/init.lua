d0c574b298d6fe2b4e5a073fc8cb568849665451
Nick Muerdter <nick.muerdter@nrel.gov>
2018-06-20 22:37:41 -0600

    Import auto-ssl mongodb adapter into repo.

--- src/api-umbrella/auto-ssl/hooks/init.lua
+++ src/api-umbrella/auto-ssl/hooks/init.lua
@@ -1,7 +1,11 @@
+local path = require "pl.path"
+
 config = require "api-umbrella.proxy.models.file_config"
 
 auto_ssl = (require "resty.auto-ssl").new({
-  hook_server_port = config["auto_ssl"]["hook_server"]["port"]
+  dir = path.join(config["etc_dir"], "auto-ssl"),
+  hook_server_port = config["auto_ssl"]["hook_server"]["port"],
+  storage_adapter = "api-umbrella.auto-ssl.storage_adapters.mongodb",
 })
 
 auto_ssl:init()
