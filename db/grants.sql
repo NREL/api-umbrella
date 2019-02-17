cc643966613af2a1be2149fc9021c257316c3bc3
Nick Muerdter <12112+GUI@users.noreply.github.com>
2019-02-16 21:57:50 -0700

    Adjust default postgres role usernames.

--- db/grants.sql
+++ db/grants.sql
@@ -1,22 +1,22 @@
 DO $$
   BEGIN
-    IF EXISTS(SELECT 1 FROM pg_roles WHERE rolname = 'api_umbrella_app_user') THEN
-      GRANT USAGE ON SCHEMA public TO api_umbrella_app_user;
-      GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO api_umbrella_app_user;
-      GRANT SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO api_umbrella_app_user;
-      REVOKE ALL ON auto_ssl_storage FROM api_umbrella_app_user;
+    IF EXISTS(SELECT 1 FROM pg_roles WHERE rolname = 'api_umbrella_app') THEN
+      GRANT USAGE ON SCHEMA public TO api_umbrella_app;
+      GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO api_umbrella_app;
+      GRANT SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO api_umbrella_app;
+      REVOKE ALL ON auto_ssl_storage FROM api_umbrella_app;
 
       IF EXISTS(SELECT 1 FROM pg_roles WHERE rolname = 'api_umbrella_owner') THEN
-        GRANT api_umbrella_app_user TO api_umbrella_owner;
+        GRANT api_umbrella_app TO api_umbrella_owner;
       END IF;
     END IF;
 
-    IF EXISTS(SELECT 1 FROM pg_roles WHERE rolname = 'api_umbrella_auto_ssl_user') THEN
-      GRANT USAGE ON SCHEMA public TO api_umbrella_auto_ssl_user;
-      GRANT SELECT, INSERT, UPDATE, DELETE ON auto_ssl_storage TO api_umbrella_auto_ssl_user;
+    IF EXISTS(SELECT 1 FROM pg_roles WHERE rolname = 'api_umbrella_auto_ssl') THEN
+      GRANT USAGE ON SCHEMA public TO api_umbrella_auto_ssl;
+      GRANT SELECT, INSERT, UPDATE, DELETE ON auto_ssl_storage TO api_umbrella_auto_ssl;
 
       IF EXISTS(SELECT 1 FROM pg_roles WHERE rolname = 'api_umbrella_owner') THEN
-        GRANT api_umbrella_auto_ssl_user TO api_umbrella_owner;
+        GRANT api_umbrella_auto_ssl TO api_umbrella_owner;
       END IF;
     END IF;
   END
