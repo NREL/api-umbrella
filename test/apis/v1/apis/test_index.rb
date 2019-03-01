57369246572a322338ada16c1600298e1bf25dc1
Nick Muerdter <12112+GUI@users.noreply.github.com>
2019-03-01 09:10:17 -0500

    Add additional tests for datatables counting with embedded records.

--- test/apis/v1/apis/test_index.rb
+++ test/apis/v1/apis/test_index.rb
@@ -12,6 +12,38 @@ class Test::Apis::V1::Apis::TestIndex < Minitest::Test
 
   include ApiUmbrellaSharedTests::DataTablesApi
 
+  def test_search_name
+    assert_data_tables_search(:name, "NameFieldSearch#{unique_test_id}Test", "amefieldsearch#{unique_test_id}t")
+  end
+
+  def test_search_frontend_host
+    assert_data_tables_search(:frontend_host, unique_test_hostname, unique_test_hostname)
+  end
+
+  def test_search_backend_host
+    assert_data_tables_search(:backend_host, unique_test_hostname, unique_test_hostname)
+  end
+
+  def test_search_server_hosts
+    assert_data_tables_search(:servers, [FactoryBot.build(:api_server, :host => "/#{unique_test_id}-server-host")], "#{unique_test_id}-server")
+  end
+
+  def test_search_url_match_frontend_prefixes
+    assert_data_tables_search(:url_matches, [FactoryBot.build(:api_url_match, :frontend_prefix => "/#{unique_test_id}-frontend-host")], "#{unique_test_id}-frontend")
+  end
+
+  def test_search_url_match_backend_prefixes
+    assert_data_tables_search(:url_matches, [FactoryBot.build(:api_url_match, :backend_prefix => "/#{unique_test_id}-backend-prefix")], "#{unique_test_id}-backend")
+  end
+
+  def test_search_counts_with_url_match_joins
+    assert_data_tables_search(:url_matches, [
+      FactoryBot.build(:api_url_match, :frontend_prefix => "/#{unique_test_id}-prefix1"),
+      FactoryBot.build(:api_url_match, :frontend_prefix => "/#{unique_test_id}-prefix2"),
+      FactoryBot.build(:api_url_match, :frontend_prefix => "/#{unique_test_id}-prefix3"),
+    ], "#{unique_test_id}-prefix")
+  end
+
   private
 
   def data_tables_api_url
