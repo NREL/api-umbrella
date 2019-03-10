56a236729fd0d3ec31deb22020a39b17e9f01f9e
Nick Muerdter <12112+GUI@users.noreply.github.com>
2019-03-09 22:47:02 -0700

    Fix DNS stale test behavior for Trafficserver 8.
    In Trafficserver 8, the behavior for stale DNS usage has changed
    slightly. If a valid NXDOMAIN result is returned, then the stale record
    is expired and will no longer be used. Instead, stale records are only
    used if the lookup actually fails. This new behavior seems fine (just
    different), So we need to change our test suite to account for these
    differences.

--- test/support/api_umbrella_test_helpers/dns.rb
+++ test/support/api_umbrella_test_helpers/dns.rb
@@ -18,10 +18,10 @@ module ApiUmbrellaTestHelpers
 
     private
 
-    def set_dns_records(records) # rubocop:disable Naming/AccessorMethodName
+    def set_dns_records(records, options = [])
       @custom_dns_records_set_during_this_test = true
       unbound_config_path = File.join($config["root_dir"], "etc/test-env/unbound/active_test.conf")
-      content = records.map { |r| "local-data: '#{r}'" }.join("\n")
+      content = (records.map { |r| "local-data: '#{r}'" } + options).join("\n")
       File.open(unbound_config_path, "w") { |f| f.write(content) }
 
       api_umbrella_process.perp_signal("test-env-unbound", "hup")
