0340c3f43884be8eb8a1ef302a878150c9fafd8b
Nick Muerdter <12112+GUI@users.noreply.github.com>
2019-11-06 12:00:51 -0700

    Fix welcome e-mail issues.
    In the postgres branch, there were a few issues with the e-mail:
    
    - Missing first "Hi," line.
    - Missing CSS styles.
    - Typo of "Account ID" in footer reading "Account Email".
    - Potential HTML escaping issues.

--- src/api-umbrella/web-app/mailers/api_user_welcome.lua
+++ src/api-umbrella/web-app/mailers/api_user_welcome.lua
@@ -8,27 +8,78 @@ local table_copy = require("pl.tablex").copy
 
 local template_html, template_html_err = etlua.compile([[
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
-<html><body>
-<p><%- greeting %></p>
-<code class="signup-key"><%- api_key %></code>
+<html>
+  <head>
+    <style type="text/css">
+      p, li, pre {
+        font-size: 15px;
+      }
 
-<% if example_api_url then %>
-<p><%- example_instruction %></p>
-<pre><a href="<%= example_api_url %>"><%- example_api_url_formatted_html %></a></pre>
-<% end %>
+      code.signup-key {
+        font-size: 18px;
+        font-weight: bold;
+        margin-bottom: 32px;
+        background-color: #f7f7f9;
+        border: 1px solid  #e1e1e8;
+        display: inline-block;
+        color: #dd1144;
+        padding: 2px 4px;
+      }
 
-<div class="signup-footer">
-  <p><%- support %></p>
-  <%- account_email %><br>
-  <%- account_id %>
-</div>
-</body></html>
+      pre {
+        background-color: #f7f7f9;
+        border: 1px solid  #e1e1e8;
+        padding: 2px 4px;
+        word-break: break-all;
+        word-wrap: break-word;
+      }
+
+      h2 {
+        font-size: 18px;
+      }
+
+      .signup-footer {
+        margin-top: 32px;
+        font-size: 12px;
+        background-color: #f5f5f5;
+        color: #555;
+        padding: 2px 4px;
+      }
+
+      .signup-footer p {
+        color: #555;
+        font-size: 12px;
+        margin: 0px 0px 8px 0px;
+        padding: 0px;
+      }
+    </style>
+  </head>
+  <body>
+    <p><%- hi %></p>
+
+    <p><%- greeting %></p>
+    <code class="signup-key"><%- api_key %></code>
+
+    <% if example_api_url then %>
+      <p><%- example_instruction %></p>
+      <pre><a href="<%= example_api_url %>"><%- example_api_url_formatted_html %></a></pre>
+    <% end %>
+
+    <div class="signup-footer">
+      <p><%- support %></p>
+      <%- account_email %><br>
+      <%- account_id %>
+    </div>
+  </body>
+</html>
 ]])
 if template_html_err then
   ngx.log(ngx.ERR, "template compile error: ", template_html_err)
 end
 
 local template_text, template_text_err = etlua.compile([[
+<%- hi %>
+
 <%- greeting %>
 
 <%- api_key %>
@@ -64,10 +115,11 @@ return function(api_user, options)
   end
 
   local data = {
+    hi = t("Hi %s,"),
     greeting = t("Your API key for %s is:"),
     api_key = api_key,
-    account_email = string.format(t("Account Email: %s"), api_user.email),
-    account_id = string.format(t("Account Email: %s"), api_user.id),
+    account_email = t("Account Email: %s"),
+    account_id = t("Account ID: %s"),
     example_api_url = options["example_api_url"],
     example_api_url_formatted_html = options["example_api_url_formatted_html"],
     example_instruction = t("You can start using this key to make web service requests. Simply pass your key in the URL when making a web request. Here's an example:"),
@@ -75,11 +127,17 @@ return function(api_user, options)
   }
 
   local data_text = table_copy(data)
+  data_text["hi"] = string.format(data["hi"], api_user.first_name)
   data_text["greeting"] = string.format(data["greeting"], api_user.email)
+  data_text["account_email"] = string.format(data["account_email"], api_user.email)
+  data_text["account_id"] = string.format(data["account_id"], api_user.id)
   data_text["support"] = string.format(data["support"], t("contact us") .. " ( " .. options["contact_url"] .. " )")
 
   local data_html = table_copy(data)
-  data_html["greeting"] = string.format(data["greeting"], "<strong>" .. api_user.email .."</strong>")
+  data_html["hi"] = string.format(data["hi"], escape_html(api_user.first_name))
+  data_html["greeting"] = string.format(data["greeting"], "<strong>" .. escape_html(api_user.email) .."</strong>")
+  data_html["account_email"] = string.format(data["account_email"], escape_html(api_user.email))
+  data_html["account_id"] = string.format(data["account_id"], escape_html(api_user.id))
   data_html["support"] = string.format(data["support"], string.format([[<a href="%s">%s</a>]], escape_html(options["contact_url"]), t("contact us")))
 
   local mailer, mailer_err = mail()
