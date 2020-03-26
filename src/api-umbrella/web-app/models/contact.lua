caed679c981126b5ed8bec46fe477f284943eafd
Nick Muerdter <12112+GUI@users.noreply.github.com>
2020-03-26 15:20:10 -0600

    Add configurable validations to the contact form API.
    So we can add validations for e-mails, subject, message body, etc.

--- src/api-umbrella/web-app/models/contact.lua
+++ src/api-umbrella/web-app/models/contact.lua
@@ -1,3 +1,4 @@
+local config = require "api-umbrella.proxy.models.file_config"
 local contact_mailer = require "api-umbrella.web-app.mailers.contact"
 local contact_policy = require "api-umbrella.web-app.policies.contact_policy"
 local is_empty = require("pl.types").is_empty
@@ -11,20 +12,24 @@ local _M = {}
 local function validate(values)
   local errors = {}
   validate_field(errors, values, "name", t("Name"), {
-    { validation_ext.string:minlen(1), t("Provide your first name.") },
+    { validation_ext.string:minlen(1), t("Provide your name.") },
+    { validation_ext.db_null_optional:not_regex(config["web"]["contact"]["name_exclude_regex"], "ijo"), t("is invalid") },
   })
   validate_field(errors, values, "email", t("Email"), {
     { validation_ext.string:minlen(1), t("Provide your email address.") },
-    { validation_ext.optional:regex([[.+@.+\..+]], "jo"), t("Provide a valid email address.") },
+    { validation_ext.optional:regex(config["web"]["contact"]["email_regex"], "ijo"), t("is invalid") },
   })
   validate_field(errors, values, "api", t("API"), {
     { validation_ext.string:minlen(1), t("Provide the API.") },
+    { validation_ext.db_null_optional:not_regex(config["web"]["contact"]["api_exclude_regex"], "ijo"), t("is invalid") },
   })
   validate_field(errors, values, "subject", t("Subject"), {
     { validation_ext.string:minlen(1), t("Provide a subject.") },
+    { validation_ext.db_null_optional:not_regex(config["web"]["contact"]["subject_exclude_regex"], "ijo"), t("is invalid") },
   })
   validate_field(errors, values, "message", t("Message"), {
     { validation_ext.string:minlen(1), t("Provide a message.") },
+    { validation_ext.db_null_optional:not_regex(config["web"]["contact"]["message_exclude_regex"], "ijo"), t("is invalid") },
   })
   return errors
 end
