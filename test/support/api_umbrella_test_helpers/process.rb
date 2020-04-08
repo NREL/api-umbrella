c51171ed715d6159ea5089278ba18a4854abdeb9
Nick Muerdter <12112+GUI@users.noreply.github.com>
2020-04-07 21:18:58 -0600

    Update dependencies.

--- test/support/api_umbrella_test_helpers/process.rb
+++ test/support/api_umbrella_test_helpers/process.rb
@@ -314,7 +314,7 @@ module ApiUmbrellaTestHelpers
             end
           end
         end
-      rescue Timeout::Error # rubocop:disable Lint/SuppressedException
+      rescue Timeout::Error
         # Ignore and return nil pid.
       end
 
