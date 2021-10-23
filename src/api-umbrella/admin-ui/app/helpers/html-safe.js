fd8a8729b22fae1e99a2c8f88fa88be19cccabff
Nick Muerdter <12112+GUI@users.noreply.github.com>
2021-10-23 11:33:51 -0600

    Fix upgrade issue.
    Somehow the upgrade from ember-simple-auth 4.0.0 to 4.0.1 breaks our
    htmlSafe helper. Not exactly sure how, but based on Ember 3 docs, I
    think this has actually been wrong for a while.

--- src/api-umbrella/admin-ui/app/helpers/html-safe.js
+++ src/api-umbrella/admin-ui/app/helpers/html-safe.js
@@ -1,9 +1,9 @@
 import { helper } from '@ember/component/helper';
-import { htmlSafe } from '@ember/string';
+import { htmlSafe } from '@ember/template';
 
 export function htmlSafeHelper(params) {
   let value = params[0];
-  return new htmlSafe(value);
+  return htmlSafe(value);
 }
 
 export default helper(htmlSafeHelper);
