372a1f3d0ea11f6557f73af06985d89d6fcb2a8b
Nick Muerdter <12112+GUI@users.noreply.github.com>
2021-10-26 11:22:56 -0600

    Have database setup process setup schema/table owners.

--- db/grants.sql
+++ db/grants.sql
@@ -1,5 +1,32 @@
+CREATE FUNCTION pg_temp.ensure_schema_owner(schema_name TEXT, role_name TEXT) RETURNS void AS $$
+  DECLARE
+    r record;
+  BEGIN
+    FOR r IN EXECUTE format('SELECT n.nspname, c.relname FROM pg_catalog.pg_class AS c JOIN pg_catalog.pg_namespace AS n ON c.relnamespace = n.oid WHERE c.relkind = ''r'' AND n.nspname = %L AND pg_get_userbyid(c.relowner) != %L', schema_name, role_name) LOOP
+      EXECUTE format('ALTER TABLE %I.%I OWNER TO %I', r.nspname, r.relname, role_name);
+    END LOOP;
+    FOR r IN EXECUTE format('SELECT n.nspname, c.relname FROM pg_catalog.pg_class AS c JOIN pg_catalog.pg_namespace AS n ON c.relnamespace = n.oid WHERE c.relkind = ''v'' AND n.nspname = %L AND pg_get_userbyid(c.relowner) != %L', schema_name, role_name) LOOP
+      EXECUTE format('ALTER VIEW %I.%I OWNER TO %I', r.nspname, r.relname, role_name);
+    END LOOP;
+    FOR r IN EXECUTE format('SELECT n.nspname, c.relname FROM pg_catalog.pg_class AS c JOIN pg_catalog.pg_namespace AS n ON c.relnamespace = n.oid WHERE c.relkind = ''m'' AND n.nspname = %L AND pg_get_userbyid(c.relowner) != %L', schema_name, role_name) LOOP
+      EXECUTE format('ALTER MATERIALIZED VIEW %I.%I OWNER TO %I', r.nspname, r.relname, role_name);
+    END LOOP;
+    FOR r IN EXECUTE format('SELECT n.nspname, c.relname FROM pg_catalog.pg_class AS c JOIN pg_catalog.pg_namespace AS n ON c.relnamespace = n.oid WHERE c.relkind = ''i'' AND n.nspname = %L AND pg_get_userbyid(c.relowner) != %L', schema_name, role_name) LOOP
+      EXECUTE format('ALTER INDEX %I.%I OWNER TO %I', r.nspname, r.relname, role_name);
+    END LOOP;
+    FOR r IN EXECUTE format('SELECT n.nspname, c.relname FROM pg_catalog.pg_class AS c JOIN pg_catalog.pg_namespace AS n ON c.relnamespace = n.oid WHERE c.relkind = ''S'' AND n.nspname = %L AND pg_get_userbyid(c.relowner) != %L', schema_name, role_name) LOOP
+      EXECUTE format('ALTER SEQUENCE %I.%I OWNER TO %I', r.nspname, r.relname, role_name);
+    END LOOP;
+  END;
+$$ LANGUAGE plpgsql;
+
 DO $$
   BEGIN
+    IF EXISTS(SELECT 1 FROM pg_roles WHERE rolname = 'api_umbrella_owner') THEN
+      PERFORM pg_temp.ensure_schema_owner('api_umbrella', 'api_umbrella_owner');
+      PERFORM pg_temp.ensure_schema_owner('audit', 'api_umbrella_owner');
+    END IF;
+
     IF EXISTS(SELECT 1 FROM pg_roles WHERE rolname = 'api_umbrella_app') THEN
       GRANT USAGE ON SCHEMA public, api_umbrella TO api_umbrella_app;
       GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public, api_umbrella TO api_umbrella_app;
