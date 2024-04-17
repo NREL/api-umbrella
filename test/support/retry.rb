b35061610d2e2d0c328b5b52682d0a0ef42bf0ea
Nick Muerdter <12112+GUI@users.noreply.github.com>
2024-04-17 12:42:06 -0600

    Add another test to retry.

--- test/support/retry.rb
+++ test/support/retry.rb
@@ -4,6 +4,7 @@ if ENV["CI"] == "true"
   Minitest::Retry.use!(
     methods_to_retry: [
       "Test::AdminUi::TestApis#test_form",
+      "Test::Proxy::TestTimeoutsResponse#test_response_closes_when_chunk_delay_exceeds_read_timeout",
     ],
 
     exceptions_to_retry: [
