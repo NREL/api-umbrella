157f40bbb0cfb3fee0fd00a8050b36a5890a733a
Nick Muerdter <12112+GUI@users.noreply.github.com>
2024-05-04 13:37:40 -0600

    Retry another test.

--- test/support/retry.rb
+++ test/support/retry.rb
@@ -3,6 +3,7 @@ if ENV["CI"] == "true"
   require "minitest/retry"
   Minitest::Retry.use!(
     methods_to_retry: [
+      "Test::AdminUi::Login::TestLocalAndExternalProviders#test_password_fields_only_for_my_account",
       "Test::AdminUi::TestApis#test_form",
       "Test::Proxy::TestTimeoutsResponse#test_response_closes_when_chunk_delay_exceeds_read_timeout",
     ],
